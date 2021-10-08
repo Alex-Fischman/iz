ecb0607f284ddbb8b3fb59caa76423a53626f191
Alex <alex@fischman.org>
2021-10-08 13:21:25 -0700

    multi-value brackets and op name replacement

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ fn main() {
     let program = std::fs::read_to_string("scratch.iz").unwrap();
 
     let (program, prefixes, affixes) = parse::preprocess(&program);
-    println!("{:?}", prefixes);
-    println!("{:?}", affixes);
+    println!("{:?} {:?}", prefixes, affixes);
+    println!("{:?}", program);
 
     let program = parse::tokenize(&program);
     program.iter().for_each(|t| print!("{:?} ", t));
@@ -18,41 +18,48 @@ fn main() {
     assert_eq!(format!("{:?}", s), "1");
 
     let s = parse::parse(&parse::tokenize("1 + 2 * 3"), &prefixes, &affixes);
-    assert_eq!(format!("{:?}", s), "(+ 1 (* 2 3))");
+    assert_eq!(format!("{:?}", s), "(add 1 (mul 2 3))");
 
     let s = parse::parse(&parse::tokenize("a + b * c * d + e"), &prefixes, &affixes);
-    assert_eq!(format!("{:?}", s), "(+ (+ a (* (* b c) d)) e)");
+    assert_eq!(format!("{:?}", s), "(add (add a (mul (mul b c) d)) e)");
 
     let s = parse::parse(&parse::tokenize("f . g . h"), &prefixes, &affixes);
-    assert_eq!(format!("{:?}", s), "(. (. f g) h)");
+    assert_eq!(format!("{:?}", s), "(dot (dot f g) h)");
 
     let s = parse::parse(&parse::tokenize("f = g = h"), &prefixes, &affixes);
-    assert_eq!(format!("{:?}", s), "(= f (= g h))");
+    assert_eq!(format!("{:?}", s), "(eq f (eq g h))");
 
-    let s = parse::parse(&parse::tokenize(" 1 + 2 + f = g = h * 3 * 4"), &prefixes, &affixes);
-    assert_eq!(format!("{:?}", s), "(= (+ (+ 1 2) f) (= g (* (* h 3) 4)))");
+    let s = parse::parse(
+        &parse::tokenize(" 1 + 2 + f = g = h * 3 * 4"),
+        &prefixes,
+        &affixes,
+    );
+    assert_eq!(
+        format!("{:?}", s),
+        "(eq (add (add 1 2) f) (eq g (mul (mul h 3) 4)))"
+    );
 
     let s = parse::parse(&parse::tokenize("- -1 * 2"), &prefixes, &affixes);
-    assert_eq!(format!("{:?}", s), "(* (- (- 1)) 2)");
+    assert_eq!(format!("{:?}", s), "(mul (neg (neg 1)) 2)");
 
     let s = parse::parse(&parse::tokenize("- -f . g"), &prefixes, &affixes);
-    assert_eq!(format!("{:?}", s), "(- (- (. f g)))");
+    assert_eq!(format!("{:?}", s), "(neg (neg (dot f g)))");
 
     let s = parse::parse(&parse::tokenize("-9!"), &prefixes, &affixes);
-    assert_eq!(format!("{:?}", s), "(- (! 9))");
+    assert_eq!(format!("{:?}", s), "(neg (fac 9))");
 
     let s = parse::parse(&parse::tokenize("f . g !"), &prefixes, &affixes);
-    assert_eq!(format!("{:?}", s), "(! (. f g))");
+    assert_eq!(format!("{:?}", s), "(fac (dot f g))");
 
     let s = parse::parse(&parse::tokenize("(((0)))"), &prefixes, &affixes);
     assert_eq!(format!("{:?}", s), "(( (( (( 0)))");
 
     let s = parse::parse(&parse::tokenize("x[0][1]"), &prefixes, &affixes);
-    assert_eq!(format!("{:?}", s), "([ ([ x 0) 1)");
+    assert_eq!(format!("{:?}", s), "(idx (idx x 0) 1)");
 
     let s = parse::parse(&parse::tokenize("if a; b; if c; d; e"), &prefixes, &affixes);
-    assert_eq!(format!("{:?}", s), "(if a b (if c d e))");
+    assert_eq!(format!("{:?}", s), "(if_ a b (if_ c d e))");
 
     let s = parse::parse(&parse::tokenize("a = if 0; b ; c = d"), &prefixes, &affixes);
-    assert_eq!(format!("{:?}", s), "(= a (= (if 0 b c) d))");
+    assert_eq!(format!("{:?}", s), "(eq a (eq (if_ 0 b c) d))");
 }
--- src/parse.rs
+++ src/parse.rs
@@ -2,31 +2,33 @@ type Bp = u8;
 type Pattern = Vec<Option<String>>;
 type Assoc = bool;
 
-type Prefixes = std::collections::HashMap<String, (u8, Pattern)>;
-type Affixes = std::collections::HashMap<String, (u8, Pattern, Assoc)>;
+type Prefixes = std::collections::HashMap<String, (String, Bp, Pattern)>;
+type Affixes = std::collections::HashMap<String, (String, Bp, Pattern, Assoc)>;
 
 pub fn preprocess(s: &str) -> (String, Prefixes, Affixes) {
     let mut prefixes = std::collections::HashMap::new();
     let mut affixes = std::collections::HashMap::new();
     let mut program = "".to_string();
     let mut last = 0;
-    for (start, _) in s.match_indices("#") {
-        let end = start + s[start..].find('\n').unwrap();
-        let comment: &Vec<&str> = &s[start..end].split(' ').filter(|s| !s.is_empty()).collect();
-        if comment[0] == "#op" {
-            let prec = comment[1].parse::<u8>().unwrap();
-            let assoc = comment[2] == "r";
-            if !assoc && comment[2] != "_" {
-                let pattern = comment[3..].iter().map(|c| if c == &"_" {None} else {Some(c.to_string())}).collect();
-                prefixes.insert(comment[2].to_string(), (prec, pattern));
+    for (i, _) in s.match_indices("#") {
+        let j = i + s[i..].find('\n').unwrap_or(s.len() - i);
+        let mut comment = s[i..j].split_whitespace();
+        if comment.next().unwrap_or("") == "#op" {
+            let func = comment.next().unwrap().to_string();
+            let bp = comment.next().unwrap().parse::<u8>().unwrap();
+            let id = comment.next().unwrap();
+            let name = comment.next().unwrap().to_string();
+            let pattern = comment
+                .map(|c| Some(c.to_string()).filter(|c| c != &"_"))
+                .collect();
+            if id == "p" {
+                prefixes.insert(name, (func, bp, pattern));
             } else {
-                let name_index = if assoc {4} else {3};
-                let pattern = comment[name_index+1..].iter().map(|c| if c == &"_" {None} else {Some(c.to_string())}).collect();
-                affixes.insert(comment[name_index].to_string(), (prec, pattern, assoc));
+                affixes.insert(name, (func, bp, pattern, id == "r"));
             }
         }
-        program.push_str(&s[last..start]);
-        last = end;
+        program.push_str(&s[last..i]);
+        last = j;
     }
     program.push_str(&s[last..]);
     (program, prefixes, affixes)
@@ -35,7 +37,8 @@ pub fn preprocess(s: &str) -> (String, Prefixes, Affixes) {
 #[derive(Clone)]
 pub struct Token(pub String, pub usize, pub TokenType);
 
-#[derive(Clone, Debug, PartialEq)]
+use TokenType::*;
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum TokenType {
     Alphabetic,
     Whitespace,
@@ -46,15 +49,8 @@ pub enum TokenType {
 }
 
 impl Token {
-    fn catagorize(c: char) -> TokenType {
-        match c {
-            _ if c.is_alphabetic() || c == '_' => TokenType::Alphabetic,
-            _ if c.is_whitespace() => TokenType::Whitespace,
-            _ if c.is_numeric() => TokenType::Numeric,
-            '(' | '{' | '[' => TokenType::Opener,
-            ')' | '}' | ']' => TokenType::Closer,
-            _ => TokenType::Other
-        }
+    fn replace(&self, s: &str) -> Token {
+        Token(s.to_string(), self.1, self.2)
     }
 }
 
@@ -67,86 +63,36 @@ impl std::fmt::Debug for Token {
 pub fn tokenize(s: &str) -> Vec<Token> {
     s.chars()
         .enumerate()
-        .map(|(i, c)| Token(c.to_string(), i, Token::catagorize(c)))
+        .map(|(i, c)| Token(c.to_string(), i, match c {
+            _ if c.is_alphabetic() || c == '_' => Alphabetic,
+            _ if c.is_whitespace() => Whitespace,
+            _ if c.is_numeric() => Numeric,
+            '(' | '{' | '[' => Opener,
+            ')' | '}' | ']' => Closer,
+            _ => Other,
+        }))
         .fold(Vec::new(), |mut acc: Vec<Token>, t| {
             match acc.last_mut() {
-                Some(s) if s.2 == t.2 && s.2 != TokenType::Opener && s.2 != TokenType::Closer => {
-                    s.0.push_str(&t.0)
-                }
+                Some(s) if s.2 == t.2 && s.2 != Opener && s.2 != Closer => s.0.push_str(&t.0),
                 _ => acc.push(t),
             }
             acc
         })
         .into_iter()
-        .filter(|t| t.2 != TokenType::Whitespace)
+        .filter(|t| t.2 != Whitespace)
         .collect()
-}
-
-pub fn parse(tokens: &Vec<Token>, prefixes: &Prefixes, affixes: &Affixes) -> S {
-    let mut prefixes = prefixes.clone();
-    prefixes.insert("-".to_string(), (5, vec![]));
-    prefixes.insert("if".to_string(), (2, vec![Some(";".to_string()), None, Some(";".to_string()), None]));
-
-    let mut affixes = affixes.clone();
-    affixes.insert("=".to_string(), (1, vec![None], true));
-    affixes.insert("+".to_string(), (3, vec![None], false));
-    affixes.insert("*".to_string(), (4, vec![None], false));
-    affixes.insert("!".to_string(), (6, vec![], false));
-    affixes.insert("[".to_string(), (7, vec![None, Some("]".to_string())], false));
-    affixes.insert(".".to_string(), (8, vec![None], false));
-
-    type Context<'a> = (&'a mut Tokens<'a>, &'a Prefixes, &'a Affixes);
-
-    fn get_right(init: S, c: &mut Context, right: &Pattern, bp: Bp, op: Token) -> S {
-        let mut v = vec![init];
-        for i in right {
-            match i {
-                Some(s) => {
-                    assert_eq!(&c.0.get().0, s);
-                    c.0.advance();
-                }
-                None => v.push(expr(c, bp)),
-            }
-        }
-        S(op, v)
-    }
-
-    fn expr(c: &mut Context, rbp: Bp) -> S {
-        let mut op = c.0.get().clone();
-        c.0.advance();
-        let mut lhs = match c.1.get(&*op.0) {
-            Some((bp, right)) => get_right(expr(c, *bp), c, &right, *bp, op),
-            None if op.2 == TokenType::Opener => {
-                let out = S(op, vec![expr(c, 0)]);
-                assert_eq!(c.0.get().2, TokenType::Closer);
-                c.0.advance();
-                out
-            }
-            None => S(op, vec![]),
-        };
-        while c.0 .0 < c.0 .1.len() {
-            op = c.0.get().clone();
-            if let Some((bp, right, assoc)) = c.2.get(&*op.0) {
-                if bp > &rbp {
-                    c.0.advance();
-                    lhs = get_right(lhs, c, &right, bp - *assoc as u8, op);
-                } else {
-                    break;
-                }
-            } else {
-                break;
-            }
-        }
-        lhs
-    }
-    expr(&mut (&mut Tokens(0, &tokens), &prefixes, &affixes), 0)
 }
 
 pub struct S(Token, Vec<S>);
 
 impl std::fmt::Debug for S {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        if self.1.is_empty() {
+        if self.0 .0.is_empty() {
+            for s in &self.1 {
+                write!(f, "{:?}", s)?;
+            }
+            Ok(())
+        } else if self.1.is_empty() {
             write!(f, "{:?}", self.0)
         } else {
             write!(f, "({:?}", self.0)?;
@@ -168,4 +114,94 @@ impl<'a> Tokens<'a> {
     fn advance(&mut self) {
         self.0 += 1
     }
+}
+
+pub fn parse(tokens: &Vec<Token>, prefixes: &Prefixes, affixes: &Affixes) -> S {
+    let mut prefixes = prefixes.clone();
+    prefixes.insert("-".to_string(), ("neg".to_string(), 5, vec![None]));
+    prefixes.insert(
+        "if".to_string(),
+        (
+            "if_".to_string(),
+            2,
+            vec![
+                None,
+                Some(";".to_string()),
+                None,
+                Some(";".to_string()),
+                None,
+            ],
+        ),
+    );
+
+    let mut affixes = affixes.clone();
+    affixes.insert("=".to_string(), ("eq".to_string(), 1, vec![None], true));
+    affixes.insert("+".to_string(), ("add".to_string(), 3, vec![None], false));
+    affixes.insert("-".to_string(), ("sub".to_string(), 3, vec![None], false));
+    affixes.insert("*".to_string(), ("mul".to_string(), 4, vec![None], false));
+    affixes.insert("!".to_string(), ("fac".to_string(), 6, vec![], false));
+    affixes.insert(
+        "[".to_string(),
+        (
+            "idx".to_string(),
+            7,
+            vec![None, Some("]".to_string())],
+            false,
+        ),
+    );
+    affixes.insert(".".to_string(), ("dot".to_string(), 8, vec![None], false));
+
+    type Context<'a> = (&'a mut Tokens<'a>, &'a Prefixes, &'a Affixes);
+
+    fn read_pattern(c: &mut Context, right: &Pattern, bp: Bp) -> Vec<S> {
+        let mut v = vec![];
+        for i in right {
+            match i {
+                Some(s) => {
+                    assert_eq!(&c.0.get().0, s);
+                    c.0.advance();
+                }
+                None => v.push(expr(c, bp)),
+            }
+        }
+        v
+    }
+
+    fn expr(c: &mut Context, rbp: Bp) -> S {
+        let mut op = c.0.get().clone();
+        c.0.advance();
+        let mut lhs = match c.1.get(&*op.0) {
+            Some((func, bp, right)) => S(op.replace(func), read_pattern(c, &right, *bp)),
+            None if op.2 == Opener => {
+                let mut v = vec![];
+                while c.0.get().2 != Closer {
+                    v.push(expr(c, 0));
+                }
+                c.0.advance();
+                S(op, v)
+            }
+            None => S(op, vec![]),
+        };
+        while c.0 .0 < c.0 .1.len() {
+            op = c.0.get().clone();
+            if let Some((func, bp, right, assoc)) = c.2.get(&*op.0) {
+                if bp > &rbp {
+                    c.0.advance();
+                    let mut v = read_pattern(c, &right, bp - *assoc as u8);
+                    v.insert(0, lhs);
+                    lhs = S(op.replace(func), v);
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        lhs
+    }
+    let mut ts = vec![Token("".to_string(), 0, Opener); tokens.len() + 2];
+    let l = ts.len() - 1;
+    ts[l] = Token("END".to_string(), 0, Closer);
+    ts[1..l].clone_from_slice(&tokens);
+    expr(&mut (&mut Tokens(0, &ts), &prefixes, &affixes), 0)
 }
