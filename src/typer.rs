fba27f060f659a01abc5b191e5b2f876b9150606
Alex <alex@fischman.org>
2022-01-27 14:31:00 -0800

    Type variables, maybe (also incomplete)

--- src/typer.rs
+++ src/typer.rs
@@ -33,41 +33,101 @@ impl std::fmt::Debug for TypedAST {
 	}
 }
 
-#[derive(Clone, Debug, PartialEq)]
+use Type::*;
+#[derive(Clone, Debug)]
 pub enum Type {
 	Bool,
 	Int,
 	Unit,
+	Opt(Box<Type>),
 	Array(Box<Type>),
 	Func(Box<Type>, Box<Type>),
+	Var(usize),
+}
+
+impl Type {
+	fn count_vars(&self) -> usize {
+		match self {
+			Bool | Int | Unit => 0,
+			Opt(t) | Array(t) => t.count_vars(),
+			Func(t, u) => t.count_vars().max(u.count_vars()),
+			Var(i) => *i + 1,
+		}
+	}
+}
+
+type Vars = [Option<Type>];
+impl PartialEq for Type {
+	fn eq(&self, other: &Type) -> bool {
+		fn eq(a: &Type, b: &Type, a_vs: &mut Vars, b_vs: &mut Vars) -> bool {
+			match (a, b) {
+				(Bool, Bool) => true,
+				(Int, Int) => true,
+				(Unit, Unit) => true,
+				(Opt(a), Opt(b)) => eq(a, b, a_vs, b_vs),
+				(Array(a), Array(b)) => eq(a, b, a_vs, b_vs),
+				(Func(a, x), Func(b, y)) => eq(a, b, a_vs, b_vs) && eq(x, y, a_vs, b_vs),
+				(Var(i), Var(j)) => match (&a_vs[*i], &b_vs[*j]) {
+					(Some(t), Some(u)) => eq(&t.clone(), &u.clone(), a_vs, b_vs),
+					(Some(t), None) => {
+						b_vs[*j] = Some(t.clone());
+						true
+					}
+					(None, Some(t)) => {
+						a_vs[*i] = Some(t.clone());
+						true
+					}
+					(None, None) => todo!(),
+				},
+				(Var(i), b) => match &a_vs[*i] {
+					Some(a) => eq(&a.clone(), b, a_vs, b_vs),
+					None => {
+						a_vs[*i] = Some(b.clone());
+						true
+					}
+				},
+				(a, Var(j)) => match &b_vs[*j] {
+					Some(b) => eq(a, &b.clone(), a_vs, b_vs),
+					None => {
+						b_vs[*j] = Some(a.clone());
+						true
+					}
+				},
+				_ => false,
+			}
+		}
+		eq(
+			self,
+			other,
+			&mut vec![None; self.count_vars()][..],
+			&mut vec![None; other.count_vars()][..],
+		)
+	}
 }
 
 pub fn annotate(ast: &AST) -> TypedAST {
-	fn unary_func(a: Type, b: Type) -> Type {
-		Type::Func(Box::new(a), Box::new(b))
-	}
 	fn binary_func(a: Type, b: Type, c: Type) -> Type {
-		Type::Func(Box::new(a), Box::new(Type::Func(Box::new(b), Box::new(c))))
+		Func(Box::new(a), Box::new(Func(Box::new(b), Box::new(c))))
 	}
 	match ast {
 		AST::Token(t) => TypedAST::Token(
 			t.clone(),
 			match &*t.string {
-				"true" | "false" => Type::Bool,
-				s if s.chars().next().unwrap().is_numeric() => Type::Int,
-				"add_int" => binary_func(Type::Int, Type::Int, Type::Int),
-				"sub_int" => binary_func(Type::Int, Type::Int, Type::Int),
-				"mul_int" => binary_func(Type::Int, Type::Int, Type::Int),
-				"neg_int" => unary_func(Type::Int, Type::Int),
-				"if_" => todo!("if statements"),
-				"else_" => todo!("else statements"),
+				"true" | "false" => Bool,
+				s if s.chars().next().unwrap().is_numeric() => Int,
+				"add_int" => binary_func(Int, Int, Int),
+				"sub_int" => binary_func(Int, Int, Int),
+				"mul_int" => binary_func(Int, Int, Int),
+				"neg_int" => Func(Box::new(Int), Box::new(Int)),
+				"if_" => binary_func(Bool, Var(0), Opt(Box::new(Var(0)))),
+				"else_" => binary_func(Opt(Box::new(Var(0))), Var(0), Var(0)),
 				s => todo!("{:?}", s),
 			},
 		),
 		AST::Call(f, x) => {
 			let f = annotate(f);
 			let x = annotate(x);
-			if let Type::Func(a, b) = f.get_type().clone() {
+			if let Func(a, b) = f.get_type().clone() {
 				assert!(*a == *x.get_type());
 				TypedAST::Call(Box::new(f), Box::new(x), *b.clone())
 			} else {
@@ -83,12 +143,12 @@ pub fn annotate(ast: &AST) -> TypedAST {
 				}
 				"{" => match xs.last() {
 					Some(x) => x.get_type().clone(),
-					None => Type::Unit,
+					None => Unit,
 				},
 				"[" => match xs.get(0).map(|x| x.get_type().clone()) {
 					Some(t) => {
 						assert!(xs.iter().all(|x| *x.get_type() == t));
-						Type::Array(Box::new(t))
+						Array(Box::new(t))
 					}
 					None => todo!(),
 				},
