420dd65354adbc030b57077e206102df5d1889ef
Alex <alex@fischman.org>
2022-01-31 23:37:22 -0800

    Unified data type representation

--- src/typer.rs
+++ src/typer.rs
@@ -18,48 +18,63 @@ impl TypedAST {
 	}
 }
 
-use Type::*;
 #[derive(Clone, PartialEq)]
-pub enum Type {
-	Unit,
-	Int,
-	Array(Box<Type>, usize),
-	Func(Box<Type>, Box<Type>),
+pub struct Type {
+	name: String,
+	size: usize,
+	args: Vec<Type>,
+}
+
+impl Type {
+	fn new(name: &str, size: usize, args: Vec<Type>) -> Type {
+		Type { name: name.to_string(), size, args }
+	}
 }
 
 impl std::fmt::Debug for Type {
 	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-		match self {
-			Unit => write!(f, "{{}}"),
-			Int => write!(f, "Int"),
-			Array(t, len) => write!(f, "[{:?}; {}]", t, len),
-			Func(x, y) => write!(f, "{:?}->{:?}", x, y),
+		match &*self.name {
+			"func" => write!(f, "{:?}->{:?}", self.args[0], self.args[1]),
+			_ => {
+				write!(f, "{}", self.name)?;
+				if !self.args.is_empty() {
+					write!(f, "@[{:?}", self.args[0])?;
+					for n in &self.args[1..] {
+						write!(f, ", {:?}", n)?;
+					}
+					write!(f, "]")?;
+				}
+				Ok(())
+			}
 		}
 	}
 }
 
 pub fn annotate(ast: &AST) -> Result<TypedAST, String> {
-	fn binary_func(a: Type, b: Type, c: Type) -> Type {
-		Func(Box::new(a), Box::new(Func(Box::new(b), Box::new(c))))
-	}
+	let int = || Type::new("int", 64, vec![]);
+	let unit = || Type::new("unit", 0, vec![]);
+	let unary_func = |a, b| Type::new("func", 64, vec![a, b]);
+	let binary_func = |a, b, c| unary_func(a, unary_func(b, c));
 	match ast {
 		AST::Token(t) => Ok(TypedAST::Token(
 			t.clone(),
 			match &*t.string {
-				s if s.chars().next().unwrap().is_numeric() => Int,
-				"_iadd_" => binary_func(Int, Int, Int),
-				"_isub_" => binary_func(Int, Int, Int),
-				"_imul_" => binary_func(Int, Int, Int),
-				"_ineg_" => Func(Box::new(Int), Box::new(Int)),
+				s if s.chars().next().unwrap().is_numeric() => int(),
+				"_iadd_" => binary_func(int(), int(), int()),
+				"_isub_" => binary_func(int(), int(), int()),
+				"_imul_" => binary_func(int(), int(), int()),
+				"_ineg_" => unary_func(int(), int()),
 				s => Err(format!("unknown token: {:?}", s))?,
 			},
 		)),
 		AST::Call(f, x) => {
 			let f = annotate(f)?;
 			let x = annotate(x)?;
-			if let Func(a, b) = f.get_type().clone() {
-				if *a == *x.get_type() {
-					Ok(TypedAST::Call(Box::new(f), Box::new(x), *b.clone()))
+			if f.get_type().name == "func" {
+				let a = &f.get_type().args[0];
+				let b = f.get_type().args[1].clone();
+				if a == x.get_type() {
+					Ok(TypedAST::Call(Box::new(f), Box::new(x), b))
 				} else {
 					Err(format!("argument type mismatch: {:?} != {:?}", a, x.get_type()))
 				}
@@ -73,26 +88,23 @@ pub fn annotate(ast: &AST) -> Result<TypedAST, String> {
 				typed_xs.push(annotate(x)?);
 			}
 			let t = match &*a.string {
-				"(" => {
-					if typed_xs.len() == 1 {
-						Ok(typed_xs[0].get_type().clone())
-					} else {
-						Err(format!("paren should only contain one expression"))
-					}
-				}
+				"(" => match typed_xs.len() {
+					1 => Ok(typed_xs[0].get_type().clone()),
+					_ => Err(format!("paren should only contain one expression")),
+				},
 				"{" => Ok(match typed_xs.last() {
 					Some(x) => x.get_type().clone(),
-					None => Unit,
+					None => unit(),
 				}),
 				"[" => match typed_xs.get(0).map(|x| x.get_type().clone()) {
 					Some(t) => {
-						if typed_xs.iter().all(|x| *x.get_type() == t) {
-							Ok(Array(Box::new(t), typed_xs.len()))
+						if typed_xs.iter().all(|x| &t == x.get_type()) {
+							Ok(Type::new("array", typed_xs.len() * t.size, vec![t]))
 						} else {
 							Err(format!("array was not heterogenous"))
 						}
 					}
-					None => todo!(),
+					_ => todo!(),
 				},
 				s => Err(format!("unknown bracket: {:?}", s)),
 			}?;
