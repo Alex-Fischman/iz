7a2c56f7adac8642e35e4c39509d1208d38cacbf
Alex <alex@fischman.org>
2022-02-20 17:55:44 -0800

    did i just write a type checker? maybe

--- src/typer.rs
+++ src/typer.rs
@@ -16,6 +16,14 @@ impl TypedAST {
 			TypedAST::List(_, _, _, t) => t,
 		}
 	}
+
+	fn set_type(&mut self, u: Type) {
+		match self {
+			TypedAST::Token(_, t) => *t = u,
+			TypedAST::Call(_, _, t) => *t = u,
+			TypedAST::List(_, _, _, t) => *t = u,
+		}
+	}
 }
 
 #[derive(Clone, PartialEq)]
@@ -42,16 +50,16 @@ impl std::fmt::Debug for Type {
 }
 
 pub fn annotate(ast: &AST) -> Result<TypedAST, String> {
-	let mut vars = 0;
-	let tree = generate_tree(ast, &mut vars)?;
-	fn generate_tree(ast: &AST, vars: &mut usize) -> Result<TypedAST, String> {
+	let mut num_vars = 0;
+	let mut tree = generate_tree(ast, &mut num_vars)?;
+	fn generate_tree(ast: &AST, num_vars: &mut usize) -> Result<TypedAST, String> {
 		let data = |s: &str| Type::Data(s.to_string(), vec![]);
 		let func = |x, y| Type::Func(Box::new(x), Box::new(y));
 		let option = |a| Type::Data("option".to_string(), vec![a]);
-		let cur_var = |vars: &mut usize| Type::Var(*vars - 1);
+		let cur_var = |num_vars: &mut usize| Type::Var(*num_vars - 1);
 		let mut new_var = || {
-			*vars += 1;
-			cur_var(vars)
+			*num_vars += 1;
+			cur_var(num_vars)
 		};
 
 		match ast {
@@ -65,20 +73,22 @@ pub fn annotate(ast: &AST) -> Result<TypedAST, String> {
 					"_ineg_" => func(data("int"), data("int")),
 					"true" => data("bool"),
 					"false" => data("bool"),
-					"_if_" => func(data("bool"), func(new_var(), option(cur_var(vars)))),
-					"_else_" => func(option(new_var()), func(cur_var(vars), cur_var(vars))),
+					"_if_" => func(data("bool"), func(new_var(), option(cur_var(num_vars)))),
+					"_else_" => {
+						func(option(new_var()), func(cur_var(num_vars), cur_var(num_vars)))
+					}
 					_ => Err(format!("unknown token: {:?}", t))?,
 				},
 			)),
 			AST::List(a, xs, b) => match &*a.string {
 				"(" => match xs.get(0) {
-					Some(x) if xs.len() == 1 => Ok(generate_tree(x, vars)?),
+					Some(x) if xs.len() == 1 => Ok(generate_tree(x, num_vars)?),
 					_ => Err(format!("paren should only contain one expression")),
 				},
 				"{" => {
 					let mut typed_xs = vec![];
 					for x in xs {
-						typed_xs.push(generate_tree(x, vars)?);
+						typed_xs.push(generate_tree(x, num_vars)?);
 					}
 					let t =
 						typed_xs.last().map(|x| x.get_type().clone()).unwrap_or(data("unit"));
@@ -88,11 +98,11 @@ pub fn annotate(ast: &AST) -> Result<TypedAST, String> {
 				s => Err(format!("unknown bracket: {:?}", s)),
 			},
 			AST::Call(f, x) => {
-				let f = generate_tree(f, vars)?;
+				let f = generate_tree(f, num_vars)?;
 				match f.get_type().clone() {
 					Type::Func(_a, b) => Ok(TypedAST::Call(
 						Box::new(f),
-						Box::new(generate_tree(x, vars)?),
+						Box::new(generate_tree(x, num_vars)?),
 						*b.clone(),
 					)),
 					t => Err(format!("expected function type but found {:?}", t)),
@@ -101,7 +111,7 @@ pub fn annotate(ast: &AST) -> Result<TypedAST, String> {
 		}
 	}
 
-	let mut constraints = vec![vec![]; vars];
+	let mut constraints = vec![vec![]; num_vars];
 	find_constraints(&tree, &mut constraints)?;
 	fn find_constraints(ast: &TypedAST, constraints: &mut Vec<Vec<Type>>) -> Result<(), String> {
 		Ok(match ast {
@@ -143,16 +153,69 @@ pub fn annotate(ast: &AST) -> Result<TypedAST, String> {
 		})
 	}
 
-	// solve constraints
-	{
-		println!("{:?}", constraints);
-		// split constraints into var-type and var-var
-		// find a type for as many vars as possible
-		// check that all var-var are satisfied
-		// check that all vars have a type
+	let mut vars: Vec<Option<Type>> = vec![None; num_vars];
+	let mut unsolved: Vec<(usize, usize)> = vec![];
+	for (i, var) in constraints.into_iter().enumerate() {
+		for constraint in var {
+			if let Type::Var(j) = constraint {
+				unsolved.push((i, j));
+			} else if let Some(t) = &vars[i] {
+				if t != &constraint {
+					Err(format!("type error:\n{:?}\n{:?}\n", t, constraint))?;
+				}
+			} else {
+				vars[i] = Some(constraint);
+			}
+		}
 	}
 
-	// update tree
+	let mut last_len = unsolved.len() + 1;
+	while last_len > unsolved.len() && !unsolved.is_empty() {
+		last_len = unsolved.len();
+
+		let mut still_unsolved = vec![];
+		for (i, j) in unsolved {
+			match (&vars[i], &vars[j]) {
+				(Some(a), Some(b)) if a != b => Err(format!("type error:\n{:?}\n{:?}\n", a, b))?,
+				(Some(_), Some(_)) => (),
+				(None, Some(b)) => vars[i] = Some(b.clone()),
+				(Some(a), None) => vars[j] = Some(a.clone()),
+				(None, None) => still_unsolved.push((i, j)),
+			}
+		}
+		unsolved = still_unsolved;
+	}
+
+	if !unsolved.is_empty() {
+		Err(format!("unresolved type constraints"))?
+	}
+
+	let vars: Vec<Type> = vars.into_iter().map(Option::unwrap).collect();
+
+	update_tree(&mut tree, &vars);
+
+	fn update_tree(ast: &mut TypedAST, vars: &[Type]) {
+		ast.set_type(update_type(ast.get_type(), vars));
+		match ast {
+			TypedAST::Token(_, _) => {}
+			TypedAST::List(_, xs, _, _) => xs.iter_mut().for_each(|x| update_tree(x, vars)),
+			TypedAST::Call(a, b, _) => {
+				update_tree(a, vars);
+				update_tree(b, vars);
+			}
+		}
+	}
+	fn update_type(t: &Type, vars: &[Type]) -> Type {
+		match t {
+			Type::Var(i) => vars[*i].clone(),
+			Type::Data(s, xs) => {
+				Type::Data(s.to_string(), xs.iter().map(|x| update_type(x, vars)).collect())
+			}
+			Type::Func(a, b) => {
+				Type::Func(Box::new(update_type(a, vars)), Box::new(update_type(b, vars)))
+			}
+		}
+	}
 
 	Ok(tree)
 }
