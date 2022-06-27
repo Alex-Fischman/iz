a25a8405ac61d08f2fc303658771e9a5bb9352ba
Alex <alex@fischman.org>
2022-06-26 21:57:20 -0700

    cleanup, basic types, basic interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,171 +1,162 @@
-use std::fmt::Debug;
-
 fn main() {
-	if let Err(e) = run() {
-		println!("error: {}", e);
-	}
-}
-
-fn run() -> Result<(), String> {
-	let args: Vec<String> = std::env::args().collect();
-	let text = match args.get(1) {
-		None => return Err("pass a file".to_string()),
+	match std::env::args().collect::<Vec<String>>().get(1) {
+		None => println!("pass a file"),
 		Some(file) => match std::fs::read_to_string(file) {
-			Err(_) => return Err("could not read file".to_string()),
-			Ok(text) => text,
+			Err(_) => println!("could not read file"),
+			Ok(text) => {
+				let tokens = tokenize(&text);
+				let ast = parse(&tokens);
+				let typed = annotate(&ast);
+				let stack = interpret(&typed);
+				println!("{:#?}", stack);
+			}
 		},
 	};
-	let tokens = tokenize(&text);
-	let ast = parse(&tokens);
-	println!("{:?}", ast);
-	Ok(())
 }
 
-#[derive(Clone, PartialEq)]
-struct Token {
-	string: String,
-	row: usize,
-	col: usize,
-}
-
-impl Debug for Token {
-	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-		write!(f, "{:?}@{}:{}", self.string, self.row, self.col)
+fn tokenize(s: &str) -> Vec<String> {
+	fn is_splitter(c: char) -> bool {
+		c == '(' || c == ')' || c == '{' || c == '}' || c.is_whitespace()
 	}
-}
-
-fn tokenize(s: &str) -> Vec<Token> {
-	fn is_bracket(c: char) -> bool {
-		c == '(' || c == ')' || c == '{' || c == '}'
-	}
-	fn char_type(c: char) -> u8 {
-		if c.is_whitespace() {
-			0
-		} else if is_bracket(c) {
-			1
-		} else {
-			2
-		}
-	}
-	let mut row = 1;
-	let mut col = 1;
-	let mut tokens: Vec<Token> = vec![];
-	let mut in_comment = false;
-	let mut in_string = false;
+	let mut tokens: Vec<String> = vec![];
 	let mut in_escape = false;
+	let mut in_string = false;
+	let mut in_comment = false;
 	for c in s.chars() {
-		if in_escape {
-			tokens.last_mut().unwrap().string.push(c);
-			in_escape = false;
-		} else if in_string && c == '\\' {
-			in_escape = true;
-		} else if in_string {
-			tokens.last_mut().unwrap().string.push(c);
-			if c == '"' {
+		match c {
+			_ if in_escape => {
+				in_escape = false;
+				tokens.last_mut().unwrap().push(c);
+			}
+			'\\' if in_string => in_escape = true,
+			'"' if in_string => {
 				in_string = false;
+				tokens.last_mut().unwrap().push(c);
 			}
-		} else if c == '"' {
-			in_string = true;
-			tokens.push(Token { string: c.to_string(), row, col });
-		} else if in_comment {
-			if c == '\n' {
-				in_comment = false;
+			_ if in_string => tokens.last_mut().unwrap().push(c),
+			'"' => {
+				in_string = true;
+				tokens.push(c.to_string());
 			}
-		} else if c == '#' {
-			in_comment = true;
-		} else if tokens.is_empty()
-			|| is_bracket(c)
-			|| char_type(c) != char_type(tokens.last().unwrap().string.chars().next().unwrap())
-		{
-			tokens.push(Token { string: c.to_string(), row, col });
-		} else {
-			tokens.last_mut().unwrap().string.push(c);
-		}
-		if c == '\n' {
-			row += 1;
-			col = 1;
-		} else {
-			col += 1;
+			'\n' if in_comment => in_comment = false,
+			_ if in_comment => {}
+			'#' => in_comment = true,
+			_ if tokens.is_empty()
+				|| is_splitter(c)
+				|| is_splitter(tokens.last().unwrap().chars().next().unwrap()) =>
+			{
+				tokens.push(c.to_string())
+			}
+			_ => tokens.last_mut().unwrap().push(c),
 		}
 	}
-	tokens.into_iter().filter(|t| !t.string.chars().next().unwrap().is_whitespace()).collect()
+	tokens.into_iter().filter(|t| !t.chars().next().unwrap().is_whitespace()).collect()
 }
 
 #[test]
 fn tokenizer_test() {
 	let result = tokenize("# Comment\n\"test \\\"str#ing\"\n toke)n1 # comment\n");
 	let target = ["\"test \"str#ing\"", "toke", ")", "n1"];
-	assert_eq!(result.iter().map(|t| &t.string).collect::<Vec<_>>(), target);
+	assert_eq!(result, target);
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 enum List {
-	Block,
 	Group,
 }
 
-#[derive(PartialEq)]
-enum Tree<Leaf, List> {
-	Leaf(Leaf),
-	List(List, Vec<Tree<Leaf, List>>),
+#[derive(Debug, PartialEq)]
+enum AST {
+	Leaf(String),
+	List(List, Vec<AST>),
 }
 
-impl<Leaf: Debug, List: Debug> Debug for Tree<Leaf, List> {
-	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-		match self {
-			Tree::Leaf(l) => write!(f, "{:?}", l),
-			Tree::List(l, v) => write!(f, "{:?} {:#?}", l, v),
+fn parse(tokens: &[String]) -> AST {
+	fn consume(tokens: &[String], index: &mut usize, end: &str) -> Vec<AST> {
+		let mut v = vec![];
+		while *index < tokens.len() && tokens[*index] != end {
+			*index += 1;
+			v.push(match &*tokens[*index - 1] {
+				"(" => AST::List(List::Group, consume(tokens, index, ")")),
+				_ => AST::Leaf(tokens[*index - 1].clone()),
+			});
 		}
+		*index += 1;
+		v
 	}
-}
-
-struct Context<'a> {
-	index: usize,
-	tokens: &'a [Token],
-}
-
-fn parse(tokens: &[Token]) -> Tree<Token, List> {
-	fn parse(c: &mut Context) -> Tree<Token, List> {
-		let t = c.tokens[c.index].clone();
-		c.index += 1;
-		if t.string == "(" {
-			let mut v = vec![];
-			while c.tokens[c.index].string != ")" {
-				v.push(parse(c));
-			}
-			c.index += 1;
-			Tree::List(List::Group, v)
-		} else if t.string == "{" {
-			let mut v = vec![];
-			while c.tokens[c.index].string != "}" {
-				v.push(parse(c));
-			}
-			c.index += 1;
-			Tree::List(List::Block, v)
-		} else {
-			Tree::Leaf(t)
-		}
-	}
-	let c = &mut Context { tokens, index: 0 };
-	let mut v = vec![];
-	while c.index < c.tokens.len() {
-		v.push(parse(c));
-	}
-	Tree::List(List::Group, v)
+	AST::List(List::Group, consume(&tokens, &mut 0, ""))
 }
 
 #[test]
 fn parser_test() {
-	let leaf = |s: &str, col| Tree::Leaf(Token { string: s.to_string(), row: 1, col });
-	let target = Tree::List(
+	let leaf = |s: &str| AST::Leaf(s.to_string());
+	let target = AST::List(
 		List::Group,
 		vec![
-			leaf("1", 1),
-			Tree::List(List::Group, vec![leaf("2", 4), leaf("5", 6), leaf("-", 8)]),
-			leaf("+", 11),
-			leaf("6", 13),
-			leaf("*", 15),
+			leaf("1"),
+			AST::List(List::Group, vec![leaf("2"), leaf("5"), leaf("-")]),
+			leaf("+"),
+			leaf("6"),
+			leaf("*"),
 		],
 	);
 	assert_eq!(parse(&tokenize("1 (2 5 -) + 6 *")), target);
+}
+
+#[derive(Debug, PartialEq)]
+enum Type {
+	Int,
+	Str,
+	Bool,
+	Block(Vec<Type>, Vec<Type>),
+}
+
+#[derive(Debug, PartialEq)]
+enum TypedAST {
+	Leaf(String, Type),
+	List(List, Vec<TypedAST>),
+}
+
+fn annotate(ast: &AST) -> TypedAST {
+	match ast {
+		AST::Leaf(s) => TypedAST::Leaf(
+			s.clone(),
+			match s.as_str() {
+				s if s.chars().all(|c| c.is_numeric() || c == '-' || c == '_') => Type::Int,
+				s if s.chars().next().unwrap() == '"' => Type::Str,
+				"true" => Type::Bool,
+				"false" => Type::Bool,
+				"add" | "sub" => Type::Block(vec![Type::Int, Type::Int], vec![Type::Int]),
+				_ => panic!("unknown token: {:?}", s),
+			},
+		),
+		AST::List(l, v) => TypedAST::List(l.clone(), v.iter().map(annotate).collect()),
+	}
+}
+
+fn interpret(ast: &TypedAST) -> Vec<i64> {
+	fn interpret(ast: &TypedAST, stack: &mut Vec<i64>) {
+		match ast {
+			TypedAST::Leaf(s, t) => match (s.as_str(), t) {
+				(s, Type::Int) => stack.push(s.parse::<i64>().unwrap()),
+				("add", Type::Block(t0, t1))
+					if *t0 == vec![Type::Int, Type::Int] && *t1 == vec![Type::Int] =>
+				{
+					let c = stack.pop().unwrap() + stack.pop().unwrap();
+					stack.push(c)
+				}
+				("sub", Type::Block(t0, t1))
+					if *t0 == vec![Type::Int, Type::Int] && *t1 == vec![Type::Int] =>
+				{
+					let c = stack.pop().unwrap() - stack.pop().unwrap();
+					stack.push(c);
+				}
+				(s, t) => panic!("unknown command: {:?}: {:?}", s, t),
+			},
+			TypedAST::List(List::Group, v) => v.iter().for_each(|t| interpret(t, stack)),
+		}
+	}
+	let mut stack = vec![];
+	interpret(ast, &mut stack);
+	stack
 }
