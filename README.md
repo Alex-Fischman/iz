b648149b45fedd3bb5a37462d86d3ce01914bc72
Alex-Fischman <alexdfischman@gmail.com>
2022-09-04 17:51:41 -0700

    tokenizer without groups implemented

--- README.md
+++ README.md
@@ -1,30 +1,6 @@
 WIP programming language
 
-# Input
-## Command Line
-Takes the name of the file to use. Must be valid UTF-8.
-#### Errors
-No argument passed, file read failure.
-## Output
-A list of characters.
-
 # Tokenizer
-## Strings
-Characters enclosed in " are treated as data instead of code.
-#### Escapes
-Inside strings, characters preceded by \ have special meanings.
-- \\\\: backslash
-- \\": double quote
-- \n: newline
-- \t: tab
-#### Errors
-Missing end quote, unknown escaped character.
-## Comments
-Characters after a # but before newlines (or the end of the file) are ignored.
-## Brackets
-(, ), {, }, [, and ] are always their own tokens.
-## Whitespace
-Tabs, spaces, and newlines are ignored.
 ## Groups
 All other characters are grouped into tokens consecutively.
 If the group is composed of only 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, and _, with an optional preceding -, then it is an integer constant.
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,136 @@
 enum Error {
-	MissingCommandLineArgument,
-	CouldNotReadFile(String),
+    MissingCommandLineArgument,
+    CouldNotReadFile(String),
+    MissingEndQuote(Location),
+	MissingEscapeCharacter(Location),
+    InvalidEscapeCharacter(Location),
 }
 
 impl std::fmt::Debug for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::MissingCommandLineArgument => writeln!(f, "no command line argument"),
-            Self::CouldNotReadFile(file) => writeln!(f, "could not read {}", file),
+            Error::MissingCommandLineArgument => writeln!(f, "no command line argument"),
+            Error::CouldNotReadFile(file) => writeln!(f, "could not read {}", file),
+            _ => todo!(),
         }
     }
 }
 
 fn main() -> Result<(), Error> {
-	let args: Vec<String> = std::env::args().collect();
-	let file = args.get(1).ok_or(Error::MissingCommandLineArgument)?;
-	let text = std::fs::read_to_string(file)
-		.map_err(|_| Error::CouldNotReadFile(file.to_owned()))?;
+    let args: Vec<String> = std::env::args().collect();
+    let file = args.get(1).ok_or(Error::MissingCommandLineArgument)?;
+    let text =
+        std::fs::read_to_string(file).map_err(|_| Error::CouldNotReadFile(file.to_owned()))?;
+    let chars: Vec<char> = text.chars().collect();
 
-	println!("{}", text);
+    let tokens = tokenize(&chars)?;
+    for token in tokens {
+        println!("{}", token.to_string(&file, &chars));
+    }
 
-	Ok(())
+    Ok(())
+}
+
+#[derive(Clone, Copy)]
+struct Location {
+    idx: usize,
+    len: usize,
+}
+
+impl Location {
+	fn new(idx: usize, len: usize) -> Location {
+		Location { idx, len }
+	}
+	
+	fn to_string(&self, chars: &[char]) -> String {
+		let mut row = 1;
+        let mut col = 1;
+        for char in chars {
+            if *char == '\n' {
+                row += 1;
+                col = 1;
+            } else {
+                col += 1;
+            }
+        }
+		format!("@{}:{}", row, col)
+	}
+}
+
+#[derive(Clone, Copy)]
+enum Bracket {
+    Round,
+    Curly,
+    Square,
+}
+
+#[derive(Clone, Copy)]
+enum TokenData {
+    Identifier,
+    String,
+    Number(i64),
+    OpenBracket(Bracket),
+    CloseBracket(Bracket),
+}
+
+struct Token(Location, TokenData);
+
+impl Token {
+    fn to_string(&self, file: &str, chars: &[char]) -> String {
+        chars[self.0.idx..self.0.idx + self.0.len]
+            .iter()
+            .collect::<String>()
+            + &self.0.to_string(chars)
+    }
+}
+
+fn tokenize(chars: &[char]) -> Result<Vec<Token>, Error> {
+    let mut out = vec![];
+    let mut i = 0;
+    while i < chars.len() {
+        match chars[i] {
+            '"' => {
+				i += 1;
+                let mut j = 0;
+                loop {
+					if i + j >= chars.len() {
+						Err(Error::MissingEndQuote(Location::new(i + j, 0)))?
+					}
+                    match chars[i + j] {
+                        '"' => break,
+                        '\\' => {
+                            j += 1;
+							if i + j >= chars.len() {
+								Err(Error::MissingEscapeCharacter(Location::new(i + j, 0)))?
+							}
+                            match chars[i + j] {
+                                '\\' | '"' | 'n' | 't' => {}
+                                _ => Err(Error::InvalidEscapeCharacter(Location::new(i + j, 1)))?
+                            }
+                        }
+                        _ => {}
+                    }
+                    j += 1;
+                }
+				out.push(Token(Location::new(i, j - 1), TokenData::String));
+                i += j;
+            }
+			'#' => {
+				i += 1;
+				while i < chars.len() && chars[i] != '\n' {
+					i += 1;
+				}
+			},
+			'(' => out.push(Token(Location::new(i, 1), TokenData::OpenBracket(Bracket::Round))),
+			'{' => out.push(Token(Location::new(i, 1), TokenData::OpenBracket(Bracket::Curly))),
+			'[' => out.push(Token(Location::new(i, 1), TokenData::OpenBracket(Bracket::Square))),
+			')' => out.push(Token(Location::new(i, 1), TokenData::CloseBracket(Bracket::Round))),
+			'}' => out.push(Token(Location::new(i, 1), TokenData::CloseBracket(Bracket::Curly))),
+			']' => out.push(Token(Location::new(i, 1), TokenData::CloseBracket(Bracket::Square))),
+			' ' | '\t' | '\n' => {},
+            _ => todo!(),
+        }
+        i += 1;
+    }
+    Ok(out)
 }
