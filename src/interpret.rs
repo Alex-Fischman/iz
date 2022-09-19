ee19c74756b69a2b67a94b24f096eee3e6619c9e
Alex Fischman <alexdfischman@gmail.com>
2022-09-19 16:42:09 -0700

    added comparison ops

--- src/interpret.rs
+++ src/interpret.rs
@@ -8,18 +8,37 @@ pub enum Value {
 	String(String),
 }
 
+impl Value {
+	fn to_int(&self) -> Result<i64, String> {
+		match self {
+			Value::Int(i) => Ok(*i),
+			v => Err(format!("expected int, found {:?}", v)),
+		}
+	}
+
+	fn to_bool(&self) -> Result<bool, String> {
+		match self {
+			Value::Bool(b) => Ok(*b),
+			v => Err(format!("expected bool, found {:?}", v)),
+		}
+	}
+
+	fn to_string(&self) -> Result<&str, String> {
+		match self {
+			Value::String(s) => Ok(s),
+			v => Err(format!("expected string, found {:?}", v)),
+		}
+	}
+}
+
 pub fn interpret(trees: &[Tree], names: &[String]) -> Result<Vec<Value>, String> {
 	fn interpret(
 		trees: &[Tree],
 		names: &[String],
 		stack: &mut Vec<Value>,
 	) -> Result<(), String> {
-		fn pop_int(stack: &mut Vec<Value>) -> Result<i64, String> {
-			match stack.pop() {
-				Some(Value::Int(i)) => Ok(i),
-				Some(v) => Err(format!("expected int, found {:?}", v)),
-				None => Err("expected int, found nothing".to_owned()),
-			}
+		fn pop(stack: &mut Vec<Value>) -> Result<Value, String> {
+			stack.pop().ok_or_else(|| "no value on stack".to_owned())
 		}
 		for tree in trees {
 			interpret(&tree.children, names, stack)?;
@@ -28,19 +47,50 @@ pub fn interpret(trees: &[Tree], names: &[String]) -> Result<Vec<Value>, String>
 					"true" => stack.push(Value::Bool(true)),
 					"false" => stack.push(Value::Bool(false)),
 					"add" => {
-						let (a, b) = (pop_int(stack)?, pop_int(stack)?);
+						let (b, a) = (pop(stack)?.to_int()?, pop(stack)?.to_int()?);
 						stack.push(Value::Int(a + b));
 					}
 					"sub" => {
-						let (a, b) = (pop_int(stack)?, pop_int(stack)?);
-						stack.push(Value::Int(b - a));
+						let (b, a) = (pop(stack)?.to_int()?, pop(stack)?.to_int()?);
+						stack.push(Value::Int(a - b));
 					}
 					"mul" => {
-						let (a, b) = (pop_int(stack)?, pop_int(stack)?);
+						let (b, a) = (pop(stack)?.to_int()?, pop(stack)?.to_int()?);
 						stack.push(Value::Int(a * b));
 					}
-					"call" | "eq" | "ne" | "lt" | "gt" | "le" | "ge" | "assign"
-					| "_if_" | "_else_" | "_while_" => todo!(),
+					"eq" => {
+						let (b, a) = (pop(stack)?, pop(stack)?);
+						stack.push(Value::Bool(match a {
+							Value::Int(a) => a == b.to_int()?,
+							Value::Bool(a) => a == b.to_bool()?,
+							Value::String(a) => a == b.to_string()?,
+						}));
+					}
+					"ne" => {
+						let (b, a) = (pop(stack)?, pop(stack)?);
+						stack.push(Value::Bool(match a {
+							Value::Int(a) => a != b.to_int()?,
+							Value::Bool(a) => a != b.to_bool()?,
+							Value::String(a) => a != b.to_string()?,
+						}));
+					}
+					"lt" => {
+						let (b, a) = (pop(stack)?.to_int()?, pop(stack)?.to_int()?);
+						stack.push(Value::Bool(a < b));
+					}
+					"gt" => {
+						let (b, a) = (pop(stack)?.to_int()?, pop(stack)?.to_int()?);
+						stack.push(Value::Bool(a > b));
+					}
+					"le" => {
+						let (b, a) = (pop(stack)?.to_int()?, pop(stack)?.to_int()?);
+						stack.push(Value::Bool(a <= b));
+					}
+					"ge" => {
+						let (b, a) = (pop(stack)?.to_int()?, pop(stack)?.to_int()?);
+						stack.push(Value::Bool(a >= b));
+					}
+					"call" | "assign" | "_if_" | "_else_" | "_while_" => todo!(),
 					s => Err(format!("unknown symbol {}", s))?,
 				},
 				Parsed::String(s) => stack.push(Value::String(s.clone())),
@@ -65,7 +115,8 @@ fn interpret_test() {
 		let (trees, names) = parse(&tokens).unwrap();
 		interpret(&trees, &names)
 	};
-	assert_eq!(f("true 1 sub"), Err("expected int, found Bool(true)".to_owned()),);
-	assert_eq!(f("1 sub"), Err("expected int, found nothing".to_owned()),);
-	assert_eq!(f("1 - 2"), Ok(vec![Value::Int(-1)]),);
+	assert_eq!(f("true 1 sub"), Err("expected int, found Bool(true)".to_owned()));
+	assert_eq!(f("1 sub"), Err("no value on stack".to_owned()));
+	assert_eq!(f("1 - 2"), Ok(vec![Value::Int(-1)]));
+	assert_eq!(f("1 > 2"), Ok(vec![Value::Bool(false)]));
 }
