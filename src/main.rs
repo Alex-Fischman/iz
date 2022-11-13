486721b4bf439eb8c81829bf47a91b9eef108049
Alex Fischman <alexdfischman@gmail.com>
2022-11-13 12:28:43 -0800

    basic input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-	
+	let args: Vec<String> = std::env::args().collect();
+	let file = args.get(1).expect("no file passed");
+	let chars: Vec<char> =
+		std::fs::read_to_string(file).expect("could not read file").chars().collect();
+	println!("{:?}", chars);
 }
