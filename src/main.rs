eb24b3e38dc3a8ddaef548e58a0d03a6a61305bb
Alex Fischman <alexdfischman@gmail.com>
2023-02-11 03:35:38 -0800

    reimplemented Any methods to add Debug restriction

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
-use std::any::Any;
-
 #[derive(Debug)]
 struct Tree {
-    data: Box<dyn Any>,
+    data: Box<dyn Data>,
     children: Vec<Tree>,
+}
+
+trait Data: std::fmt::Debug + std::any::Any {}
+impl<T: std::fmt::Debug + std::any::Any> Data for T {}
+
+impl dyn Data {
+    fn is<T: std::any::Any>(&self) -> bool {
+        self.type_id() == std::any::TypeId::of::<T>()
+    }
+
+    fn xx<T: std::any::Any>(&self) -> Option<&T> {
+        if self.is::<T>() {
+            unsafe { Some(&*(self as *const dyn Data as *const T)) }
+        } else {
+            None
+        }
+    }
+
+    fn xxx<T: std::any::Any>(&mut self) -> Option<&mut T> {
+        if self.is::<T>() {
+            unsafe { Some(&mut *(self as *mut dyn Data as *mut T)) }
+        } else {
+            None
+        }
+    }
+}
+
+fn postorder<F: Fn(&mut Vec<Tree>) + Clone>(tree: &mut Tree, f: F) {
+    for tree in &mut tree.children {
+        postorder(tree, f.clone());
+    }
+    f(&mut tree.children);
 }
 
 fn main() {
@@ -22,38 +52,40 @@ fn main() {
         children: tokens,
     };
 
-    postorder(&mut tree, remove_whitespace);
+    // group identifiers
+    postorder(&mut tree, |trees| {
+        let mut i = 0;
+        while i < trees.len() {
+            match trees[i].data.xx::<char>().cloned() {
+                Some(c) if matches!(c, '_' | 'a' ..= 'z' | 'A' ..= 'Z' | '0' ..= '9') => {
+                    if i == 0 {
+                        trees[i].data = Box::new(c.to_string());
+                    } else if let Some(s) = trees[i - 1].data.xxx::<String>() {
+                        s.push(c);
+                        trees.remove(i);
+                    } else {
+                        trees[i].data = Box::new(c.to_string());
+                    }
+                }
+                _ => i += 1,
+            }
+        }
+    });
+
+    // remove whitespace
+    postorder(&mut tree, |trees| {
+        let mut i = 0;
+        while i < trees.len() {
+            match trees[i].data.xx::<char>() {
+                Some(c) if c.is_whitespace() => {
+                    trees.remove(i);
+                }
+                _ => i += 1,
+            }
+        }
+    });
 
     println!("{tree:#?}");
-}
-
-fn remove_whitespace(trees: &mut Vec<Tree>) {
-    let mut i = 0;
-    while i < trees.len() {
-        match trees[i].data.downcast_ref::<char>() {
-            Some(c) if c.is_whitespace() => {
-                trees.remove(i);
-            }
-            _ => i += 1,
-        }
-    }
-}
-
-// #[derive(Clone, Debug, PartialEq)]
-// enum Leaf {
-//     CurlyLeft,
-//     CurlyRight,
-//     Identifier(String),
-//     Integer(i64),
-//     Whitespace(char),
-//     Char(char),
-// }
-
-fn postorder<F: Fn(&mut Vec<Tree>) + Clone>(tree: &mut Tree, f: F) {
-    for tree in &mut tree.children {
-        postorder(tree, f.clone());
-    }
-    f(&mut tree.children);
 }
 
 #[allow(dead_code)]
