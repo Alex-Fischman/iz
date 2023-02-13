8a146cd788600963a45ea15b4046a864554fbd6c
Alex Fischman <alexdfischman@gmail.com>
2023-02-13 12:54:48 -0800

    switched to using normal enum

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,16 @@
-use std::any::Any;
-use std::fmt::Debug;
-
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct Tree {
-    data: Box<dyn Data>,
+    data: Data,
     children: Vec<Tree>,
 }
 
-trait Data: Any + Debug {
-    fn as_any(&self) -> &dyn Any;
-    fn as_any_mut(&mut self) -> &mut dyn Any;
-}
-
-impl<T: Any + Debug> Data for T {
-    fn as_any(&self) -> &dyn Any {
-        self
-    }
-
-    fn as_any_mut(&mut self) -> &mut dyn Any {
-        self
-    }
-}
-
-impl dyn Data {
-    fn is<T: Any>(&self) -> bool {
-        self.as_any().is::<T>()
-    }
-
-    fn xx<T: Any>(&self) -> Option<&T> {
-        self.as_any().downcast_ref::<T>()
-    }
-
-    fn xxx<T: Any>(&mut self) -> Option<&mut T> {
-        self.as_any_mut().downcast_mut::<T>()
-    }
+#[derive(Debug, PartialEq)]
+enum Data {
+    Empty,
+    Char(char),
+    String(String),
+    Int(i64),
+    Op(Op),
 }
 
 fn postorder<F: Fn(&mut Vec<Tree>) + Clone>(tree: &mut Tree, f: F) {
@@ -50,12 +27,12 @@ fn main() {
         .expect("could not read file")
         .chars()
         .map(|c| Tree {
-            data: Box::new(c),
+            data: Data::Char(c),
             children: vec![],
         })
         .collect();
     let mut tree = Tree {
-        data: Box::new(()),
+        data: Data::Empty,
         children: tokens,
     };
 
@@ -65,10 +42,10 @@ fn main() {
     postorder(&mut tree, |trees| {
         let mut i = 0;
         while i < trees.len() {
-            match trees[i].data.xx::<char>() {
-                Some('#') => {
+            match trees[i].data {
+                Data::Char('#') => {
                     let mut j = i;
-                    while j < trees.len() && trees[j].data.xx::<char>() != Some(&'\n') {
+                    while j < trees.len() && !matches!(trees[j].data, Data::Char('\n')) {
                         j += 1;
                     }
                     trees.drain(i..j);
@@ -82,13 +59,15 @@ fn main() {
     postorder(&mut tree, |trees| {
         let mut i = 0;
         while i < trees.len() {
-            match trees[i].data.xx::<char>().cloned() {
-                Some(c) if matches!(c, '_' | 'a' ..= 'z' | 'A' ..= 'Z' | '0' ..= '9') => {
-                    if i == 0 || !trees[i - 1].data.is::<String>() {
-                        trees[i].data = Box::new(c.to_string());
-                    } else {
-                        trees[i - 1].data.xxx::<String>().unwrap().push(c);
+            match trees[i].data {
+                Data::Char(c @ ('_' | 'a'..='z' | 'A'..='Z' | '0'..='9')) => {
+                    if i == 0 {
+                        trees[i].data = Data::String(c.to_string());
+                    } else if let Data::String(s) = &mut trees[i - 1].data {
+                        s.push(c);
                         trees.remove(i);
+                    } else {
+                        trees[i].data = Data::String(c.to_string());
                     }
                 }
                 _ => i += 1,
@@ -100,8 +79,8 @@ fn main() {
     postorder(&mut tree, |trees| {
         let mut i = 0;
         while i < trees.len() {
-            match trees[i].data.xx::<char>() {
-                Some(c) if c.is_whitespace() => {
+            match trees[i].data {
+                Data::Char(c) if c.is_whitespace() => {
                     trees.remove(i);
                 }
                 _ => i += 1,
@@ -113,8 +92,11 @@ fn main() {
     postorder(&mut tree, |trees| {
         let mut i = 0;
         while i < trees.len() {
-            match trees[i].data.xx::<String>().map(|s| s.parse::<i64>()) {
-                Some(Ok(int)) => trees[i].data = Box::new(int),
+            match &trees[i].data {
+                Data::String(s) => match s.parse::<i64>() {
+                    Ok(int) => trees[i].data = Data::Int(int),
+                    _ => i += 1,
+                },
                 _ => i += 1,
             }
         }
@@ -124,19 +106,22 @@ fn main() {
     postorder(&mut tree, |trees| {
         let mut i = 0;
         while i < trees.len() {
-            if let Some(int) = trees[i].data.xx::<i64>() {
-                trees[i].data = Box::new(Op::Psh(*int))
-            } else if let Some(s) = trees[i].data.xx::<String>() {
-                trees[i].data = Box::new(match s.as_str() {
+            if let Data::Int(int) = trees[i].data {
+                trees[i].data = Data::Op(Op::Psh(int))
+            } else if let Data::String(s) = &trees[i].data {
+                trees[i].data = Data::Op(match s.as_str() {
                     "add" => Op::Add,
                     "neg" => Op::Neg,
                     "ltz" => Op::Ltz,
                     "lbl" => {
                         i += 1;
-                        Op::Lbl(*trees[i].data.xx::<i64>().unwrap())
+                        Op::Lbl(match &trees[i].data {
+                            Data::Int(int) => *int,
+                            data => panic!("expected int, found {data:?}"),
+                        })
                     }
                     "jmp" => Op::Jmp,
-                    _ => panic!("unknown op"),
+                    op => panic!("unknown op {op:?}"),
                 })
             } else {
                 panic!("unknown op")
@@ -149,13 +134,16 @@ fn main() {
 
     // interpreter backend
 
-    assert!(tree.data.is::<()>());
+    assert_eq!(tree.data, Data::Empty);
     let program: Vec<Op> = tree
         .children
-        .iter()
+        .into_iter()
         .map(|tree| {
             assert!(tree.children.is_empty());
-            tree.data.xx::<Op>().unwrap().clone()
+            match tree.data {
+                Data::Op(op) => op,
+                data => panic!("expected op, found {data:?}"),
+            }
         })
         .collect();
 
@@ -200,7 +188,7 @@ fn main() {
     println!("{stack:?}");
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 enum Op {
     Psh(i64),
     Add,
@@ -210,7 +198,7 @@ enum Op {
     Jmp,
 }
 
-impl Debug for Op {
+impl std::fmt::Debug for Op {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             Op::Psh(i) => write!(f, "psh {i}"),
