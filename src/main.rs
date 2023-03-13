63b704a843e9718b06bd7d1c3bfe7fe0344385be
Alex Fischman <alexdfischman@gmail.com>
2023-03-12 20:02:59 -0700

    cleaned up

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::{
     clone::Clone,
     cmp::Eq,
     collections::HashMap,
+    default::Default,
     env::args,
     fs::read_to_string,
     hash::Hash,
@@ -32,19 +33,11 @@ struct IndexMap<K: Key, V> {
 }
 
 impl<K: Key, V> IndexMap<K, V> {
-    fn new() -> IndexMap<K, V> {
-        IndexMap {
-            idxs: HashMap::new(),
-            keys: Vec::new(),
-            vals: Vec::new(),
-        }
-    }
-
-    fn value(&self, key: &K) -> Option<&V> {
+    fn get(&self, key: &K) -> Option<&V> {
         self.idxs.get(key).map(|i| &self.vals[*i])
     }
 
-    fn value_mut(&mut self, key: &K) -> Option<&mut V> {
+    fn get_mut(&mut self, key: &K) -> Option<&mut V> {
         self.idxs.get(key).map(|i| &mut self.vals[*i])
     }
 
@@ -59,14 +52,26 @@ impl<K: Key, V> IndexMap<K, V> {
             }
         }
     }
+}
 
-    fn or_insert(&mut self, key: K, val: V) -> &mut V {
+impl<K: Key, V: Default> IndexMap<K, V> {
+    fn or_default(&mut self, key: K) -> &mut V {
         match self.idxs.get(&key) {
             Some(i) => &mut self.vals[*i],
             None => {
-                self.insert(key.clone(), val);
-                self.value_mut(&key).unwrap()
+                self.insert(key.clone(), V::default());
+                self.get_mut(&key).unwrap()
             }
+        }
+    }
+}
+
+impl<K: Key, V> Default for IndexMap<K, V> {
+    fn default() -> IndexMap<K, V> {
+        IndexMap {
+            idxs: HashMap::default(),
+            keys: Vec::default(),
+            vals: Vec::default(),
         }
     }
 }
@@ -75,26 +80,25 @@ struct Graph<T: Key>(IndexMap<T, IndexMap<T, ()>>);
 
 impl<T: Key> Graph<T> {
     fn node(&mut self, node: T) -> Option<IndexMap<T, ()>> {
-        self.0.insert(node, IndexMap::new())
+        self.0.insert(node, IndexMap::default())
     }
 
     fn edge(&mut self, parent: T, child: T) {
-        self.0.or_insert(parent, IndexMap::new()).insert(child, ());
+        self.0.or_default(parent).insert(child, ());
     }
 
     fn children(&self, parent: T) -> &[T] {
-        &self.0.value(&parent).unwrap().keys
+        &self.0.get(&parent).unwrap().keys
     }
 
     fn children_mut(&mut self, parent: T) -> &mut Vec<T> {
-        &mut self.0.value_mut(&parent).unwrap().keys
+        &mut self.0.get_mut(&parent).unwrap().keys
     }
 }
 
 struct Context<'a> {
     file: &'a str,
     text: &'a str,
-
     id: usize,
     graph: Graph<usize>,
     tokens: HashMap<usize, &'a str>,
@@ -105,9 +109,8 @@ impl<'a> Context<'a> {
         Context {
             file,
             text,
-
             id: 0,
-            graph: Graph(IndexMap::new()),
+            graph: Graph(IndexMap::default()),
             tokens: HashMap::new(),
         }
     }
@@ -133,7 +136,7 @@ impl<'a> Context<'a> {
                 col += 1;
             }
         }
-        Some(format!(" at {}:{}:{}", self.file, row, col))
+        Some(format!("{}:{}:{}", self.file, row, col))
     }
 
     // if self.graph is a tree, will work as expected
@@ -142,7 +145,7 @@ impl<'a> Context<'a> {
     fn print_tree(&self, root: usize, indent: usize) {
         print!("{}{:05}", "------- ".repeat(indent), root,);
         if let Some(location) = self.location(root) {
-            print!(" @ {}", location);
+            print!(" at {}", location);
         }
         if let Some(token) = self.tokens.get(&root) {
             print!("\t{:?}", token);
@@ -213,7 +216,7 @@ fn remove_comments(context: &mut Context) {
         if c == &"#" {
             let mut j = i;
             while j < context.graph.children(0).len()
-                && context.tokens.get(&context.graph.children(0)[j]) != Some(&"\n")
+                && context.tokens[&context.graph.children(0)[j]] != "\n"
             {
                 context.tokens.remove(&context.graph.children(0)[j]);
                 j += 1;
@@ -252,8 +255,8 @@ fn group_tokens(context: &mut Context) {
     let mut i = 1;
     let children = context.graph.children_mut(0);
     while i < children.len() {
-        let curr = context.tokens.get(&children[i]).unwrap();
-        let prev = context.tokens.get(&children[i - 1]).unwrap();
+        let curr = context.tokens[&children[i]];
+        let prev = context.tokens[&children[i - 1]];
         // don't want ((s       can consume #s
         if !is_bracket(curr) && !is_whitespace(curr) && token_type(curr) == token_type(prev) {
             context.tokens.insert(children[i - 1], unsafe {
@@ -274,7 +277,7 @@ fn remove_whitespace(context: &mut Context) {
     let mut i = 0;
     let children = context.graph.children_mut(0);
     while i < children.len() {
-        if is_whitespace(context.tokens.get(&children[i]).unwrap()) {
+        if is_whitespace(context.tokens[&children[i]]) {
             context.tokens.remove(&children[i]).unwrap();
             children.remove(i);
         } else {
