fbe8e207a03d7ed30fd0dbf82a83981742ee654d
Alex Fischman <alexdfischman@gmail.com>
2023-03-13 22:02:09 -0700

    removed Map; it was def. faster but not worth it ergo-wise

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,11 @@ extern crate std;
 
 use std::{
     clone::Clone,
-    cmp::Eq,
     collections::HashMap,
     env::args,
     fs::read_to_string,
-    hash::Hash,
     iter::Iterator,
-    iter::{ExactSizeIterator, IntoIterator},
-    ops::RangeBounds,
-    option::{Option, Option::None, Option::Some},
+    option::{Option::None, Option::Some},
     string::String,
     vec::Vec,
     {format, matches, print},
@@ -22,105 +18,6 @@ macro_rules! panic {
     () => {{ std::process::exit(-1); }};
     ($fmt:literal) => {{ std::eprint!($fmt); std::process::exit(-1); }};
     ($fmt:literal, $($arg:tt)*) => {{ std::eprint!($fmt, $($arg)*); std::process::exit(-1); }};
-}
-
-pub trait Key: Clone + Eq + Hash {}
-impl<T: Clone + Eq + Hash> Key for T {}
-
-// has fast lookup from HashMap and ordering from Vec
-pub struct Map<K: Key, V> {
-    idxs: HashMap<K, usize>,
-    keys: Vec<K>,
-    vals: Vec<V>,
-}
-
-impl<K: Key, V> Map<K, V> {
-    pub fn new() -> Map<K, V> {
-        Map {
-            idxs: HashMap::new(),
-            keys: Vec::new(),
-            vals: Vec::new(),
-        }
-    }
-
-    pub fn keys(&self) -> &[K] {
-        &self.keys
-    }
-
-    pub fn get(&self, key: &K) -> Option<&V> {
-        self.idxs.get(key).map(|i| &self.vals[*i])
-    }
-
-    pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
-        self.idxs.get(key).map(|i| &mut self.vals[*i])
-    }
-
-    pub fn contains_key(&self, key: &K) -> bool {
-        self.idxs.contains_key(key)
-    }
-
-    pub fn insert(&mut self, key: K, val: V) -> Option<V> {
-        match self.idxs.get(&key) {
-            Some(i) => Some(std::mem::replace(&mut self.vals[*i], val)),
-            None => {
-                self.idxs.insert(key.clone(), self.keys.len());
-                self.keys.push(key);
-                self.vals.push(val);
-                None
-            }
-        }
-    }
-
-    pub fn splice<'a, R, A, X, B, Y>(&'a mut self, range: R, keys: A, vals: B) -> (Vec<K>, Vec<V>)
-    where
-        R: RangeBounds<usize> + Clone,
-        A: IntoIterator<IntoIter = X>,
-        B: IntoIterator<IntoIter = Y>,
-        X: ExactSizeIterator<Item = K> + 'a,
-        Y: ExactSizeIterator<Item = V> + 'a,
-    {
-        let keys = keys.into_iter();
-        let vals = vals.into_iter();
-        if keys.len() != vals.len() {
-            panic!("keys and vals had different lengths")
-        }
-        let keys = self.keys.splice(range.clone(), keys).collect();
-        let vals = self.vals.splice(range, vals).collect();
-        // completely rebuild idxs; has to be linear anyway
-        self.idxs = self
-            .keys
-            .iter()
-            .enumerate()
-            .map(|(i, key)| (key.clone(), i))
-            .collect();
-        (keys, vals)
-    }
-}
-
-pub struct Graph<N: Key, E>(Map<N, Map<N, E>>);
-
-impl<N: Key, E> Graph<N, E> {
-    pub fn new() -> Graph<N, E> {
-        Graph(Map::new())
-    }
-
-    pub fn node(&mut self, node: N) {
-        self.0.insert(node, Map::new());
-    }
-
-    pub fn edge(&mut self, parent: N, child: N, edge: E) {
-        assert!(self.0.contains_key(&parent), "missing parent");
-        assert!(self.0.contains_key(&child), "missing child");
-        self.0.get_mut(&parent).unwrap().insert(child, edge);
-    }
-
-    pub fn children(&self, parent: &N) -> &Map<N, E> {
-        self.0.get(parent).unwrap()
-    }
-
-    pub fn children_mut(&mut self, parent: &N) -> &mut Map<N, E> {
-        self.0.get_mut(parent).unwrap()
-    }
 }
 
 // package these to make Token as small as possible
@@ -159,17 +56,23 @@ impl Token<'_> {
     }
 }
 
+type Graph<T> = HashMap<T, Vec<T>>;
+
 struct Context<'a> {
-    graph: Graph<usize, ()>,
+    graph: Graph<usize>,
     tokens: Vec<Token<'a>>,
 }
 
 impl<'a> Context<'a> {
     fn node(&mut self, token: Token<'a>) -> usize {
         let node = self.tokens.len();
-        self.graph.node(node);
+        self.graph.insert(node, Vec::new());
         self.tokens.push(token);
         node
+    }
+
+    fn children_mut(&mut self, parent: usize) -> &mut Vec<usize> {
+        self.graph.get_mut(&parent).unwrap()
     }
 
     // if self.graph is a tree, will work as expected
@@ -181,7 +84,7 @@ impl<'a> Context<'a> {
             print!(" at {}\t{:?}", token.location(), token.as_str());
         }
         print!("\n");
-        for child in self.graph.children(&root).keys() {
+        for child in &self.graph[&root] {
             self.print_tree(*child, indent + 1);
         }
     }
@@ -215,7 +118,7 @@ fn main() {
             lo: i,
             hi: j,
         });
-        c.graph.edge(root, node, ());
+        c.children_mut(root).push(node);
     }
 
     let passes = [
@@ -239,29 +142,28 @@ fn main() {
         // interpret,
     ];
     for pass in passes {
-        pass(&mut c, &root)
+        pass(&mut c, root)
     }
 
     c.print_tree(root, 0);
 }
 
-fn remove_comments(c: &mut Context, root: &usize) {
+fn remove_comments(c: &mut Context, root: usize) {
     let mut i = 0;
-    let children = c.graph.children_mut(root);
-    while i < children.keys().len() {
-        if c.tokens[children.keys()[i]].as_str() == "#" {
+    while i < c.graph[&root].len() {
+        if c.tokens[c.graph[&root][i]].as_str() == "#" {
             let mut j = i;
-            while j < children.keys().len() && c.tokens[children.keys()[j]].as_str() != "\n" {
+            while j < c.graph[&root].len() && c.tokens[c.graph[&root][j]].as_str() != "\n" {
                 j += 1;
             }
-            children.splice(i..=j, [], []);
+            c.graph.get_mut(&root).unwrap().drain(i..=j);
         } else {
             i += 1;
         }
     }
 }
 
-fn group_tokens(c: &mut Context, root: &usize) {
+fn group_tokens(c: &mut Context, root: usize) {
     let is_bracket = |s: &str| matches!(s, "(" | ")" | "{" | "}" | "[" | "]");
     let token_type = |s: &str| {
         if s.chars()
@@ -278,40 +180,38 @@ fn group_tokens(c: &mut Context, root: &usize) {
     };
 
     let mut i = 1;
-    let children = c.graph.children_mut(root);
-    while i < children.keys().len() {
-        let curr = &c.tokens[children.keys()[i]];
-        let prev = &c.tokens[children.keys()[i - 1]];
+    while i < c.graph[&root].len() {
+        let curr = &c.tokens[c.graph[&root][i]];
+        let prev = &c.tokens[c.graph[&root][i - 1]];
         if !is_bracket(curr.as_str())
             && token_type(curr.as_str()) == token_type(prev.as_str())
             && curr.source == prev.source
             && prev.hi == curr.lo
         {
-            c.tokens.get_mut(children.keys()[i - 1]).unwrap().hi = curr.hi;
-            children.splice(i..=i, [], []);
+            c.tokens.get_mut(c.graph[&root][i - 1]).unwrap().hi = curr.hi;
+            c.graph.get_mut(&root).unwrap().remove(i);
         } else {
             i += 1;
         }
     }
 }
 
-fn remove_whitespace(c: &mut Context, root: &usize) {
+fn remove_whitespace(c: &mut Context, root: usize) {
     let mut i = 0;
-    let children = c.graph.children_mut(root);
-    while i < children.keys().len() {
-        if c.tokens[children.keys()[i]]
+    while i < c.graph[&root].len() {
+        if c.tokens[c.graph[&root][i]]
             .as_str()
             .chars()
             .all(char::is_whitespace)
         {
-            children.splice(i..=i, [], []);
+            c.graph.get_mut(&root).unwrap().remove(i);
         } else {
             i += 1;
         }
     }
 }
 
-fn group_brackets(c: &mut Context, root: &usize) {
+fn group_brackets(c: &mut Context, root: usize) {
     let match_opener = |token: &Token| match token.as_str() {
         "(" => ")",
         "{" => "}",
@@ -320,8 +220,8 @@ fn group_brackets(c: &mut Context, root: &usize) {
     };
     let mut stack = Vec::new();
     let mut i = 0;
-    while i < c.graph.children(root).keys().len() {
-        let child = c.graph.children(root).keys()[i];
+    while i < c.graph[&root].len() {
+        let child = c.graph[&root][i];
         let token = &c.tokens[child];
         match token.as_str() {
             "(" | "{" | "[" => {
@@ -333,7 +233,7 @@ fn group_brackets(c: &mut Context, root: &usize) {
                 Some(popped) => {
                     let opener = &c.tokens[popped];
                     if match_opener(opener) == token.as_str() {
-                        c.graph.children_mut(root).splice(i..=i, [], []);
+                        c.children_mut(root).remove(i);
                     } else {
                         panic!(
                             "{} matched with {} at {} and {}\n",
@@ -347,9 +247,8 @@ fn group_brackets(c: &mut Context, root: &usize) {
             },
             _ if stack.is_empty() => i += 1,
             _ => {
-                let vs = c.graph.children_mut(root).splice(i..=i, [], []);
-                let top = c.graph.children_mut(stack.last().unwrap());
-                top.splice(top.keys().len().., vs.0, vs.1);
+                let child = c.children_mut(root).remove(i);
+                c.children_mut(*stack.last().unwrap()).push(child);
             }
         }
     }
