e85dc8bc9e34eb89696b9e1b618d20289089f2a5
Alex Fischman <alexdfischman@gmail.com>
2023-03-13 22:28:33 -0700

    make graph better hopefully

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ extern crate std;
 
 use std::{
     clone::Clone,
-    collections::HashMap,
     env::args,
     fs::read_to_string,
     iter::Iterator,
@@ -55,35 +54,65 @@ impl Token<'_> {
     }
 }
 
-type Graph<T> = HashMap<T, Vec<T>>;
+use graph::{Graph, Node};
+mod graph {
+    extern crate std;
+    use std::{collections::HashMap, vec::Vec};
 
-struct Context<'a> {
-    graph: Graph<usize>,
-    tokens: Vec<Token<'a>>,
-}
-
-impl<'a> Context<'a> {
-    fn node(&mut self, token: Token<'a>) -> usize {
-        let node = self.tokens.len();
-        self.graph.insert(node, Vec::new());
-        self.tokens.push(token);
-        node
+    #[derive(Clone, Copy, Eq, Hash, PartialEq)]
+    pub struct Node(usize);
+    pub struct Graph<T> {
+        nodes: Vec<T>,
+        edges: HashMap<Node, Vec<Node>>,
     }
 
-    // if self.graph is a tree, will work as expected
-    // if self.graph is a DAG, will print shared nodes multiple times
-    // if self.graph has cycles, will loop forever
-    fn print_tree(&self, root: usize, indent: usize) {
-        println!(
-            "{}{:5} at {}:\t{}",
-            "\t".repeat(indent),
-            root,
-            self.tokens[root].location(),
-            self.tokens[root].as_str(),
-        );
-        for child in &self.graph[&root] {
-            self.print_tree(*child, indent + 1);
+    impl<T> Graph<T> {
+        pub fn new() -> Graph<T> {
+            Graph {
+                nodes: Vec::new(),
+                edges: HashMap::new(),
+            }
         }
+
+        pub fn node(&mut self, x: T) -> Node {
+            let node = self.nodes.len();
+            self.nodes.push(x);
+            self.edges.insert(Node(node), Vec::new());
+            Node(node)
+        }
+
+        pub fn data(&self, node: Node) -> &T {
+            &self.nodes[node.0]
+        }
+
+        pub fn data_mut(&mut self, node: Node) -> &mut T {
+            &mut self.nodes[node.0]
+        }
+
+        pub fn children(&self, node: Node) -> &[Node] {
+            &self.edges[&node]
+        }
+
+        pub fn children_mut(&mut self, node: Node) -> &mut Vec<Node> {
+            self.edges.get_mut(&node).unwrap()
+        }
+    }
+}
+
+type Context<'a> = Graph<Token<'a>>;
+
+// if self.graph is a tree, will work as expected
+// if self.graph is a DAG, will print shared nodes multiple times
+// if self.graph has cycles, will loop forever
+fn print_tree(c: &Context, root: &Node, indent: usize) {
+    println!(
+        "{}at {}:\t{}",
+        "\t".repeat(indent),
+        c.data(*root).location(),
+        c.data(*root).as_str(),
+    );
+    for child in c.children(*root) {
+        print_tree(c, child, indent + 1);
     }
 }
 
@@ -97,10 +126,7 @@ fn main() {
         text,
     };
 
-    let mut c = Context {
-        graph: Graph::new(),
-        tokens: Vec::new(),
-    };
+    let mut c = Graph::new();
     let root = c.node(Token {
         source,
         lo: 0,
@@ -115,7 +141,7 @@ fn main() {
             lo: i,
             hi: j,
         });
-        c.graph.get_mut(&root).unwrap().push(node);
+        c.children_mut(root).push(node);
     }
 
     let passes = [
@@ -142,25 +168,25 @@ fn main() {
         pass(&mut c, root)
     }
 
-    c.print_tree(root, 0);
+    print_tree(&c, &root, 0);
 }
 
-fn remove_comments(c: &mut Context, root: usize) {
+fn remove_comments(c: &mut Context, root: Node) {
     let mut i = 0;
-    while i < c.graph[&root].len() {
-        if c.tokens[c.graph[&root][i]].as_str() == "#" {
+    while i < c.children(root).len() {
+        if c.data(c.children(root)[i]).as_str() == "#" {
             let mut j = i;
-            while j < c.graph[&root].len() && c.tokens[c.graph[&root][j]].as_str() != "\n" {
+            while j < c.children(root).len() && c.data(c.children(root)[j]).as_str() != "\n" {
                 j += 1;
             }
-            c.graph.get_mut(&root).unwrap().drain(i..=j);
+            c.children_mut(root).drain(i..=j);
         } else {
             i += 1;
         }
     }
 }
 
-fn group_tokens(c: &mut Context, root: usize) {
+fn group_tokens(c: &mut Context, root: Node) {
     let is_bracket = |s: &str| matches!(s, "(" | ")" | "{" | "}" | "[" | "]");
     let token_type = |s: &str| {
         if s.chars()
@@ -177,38 +203,38 @@ fn group_tokens(c: &mut Context, root: usize) {
     };
 
     let mut i = 1;
-    while i < c.graph[&root].len() {
-        let curr = &c.tokens[c.graph[&root][i]];
-        let prev = &c.tokens[c.graph[&root][i - 1]];
+    while i < c.children(root).len() {
+        let curr = &c.data(c.children(root)[i]);
+        let prev = &c.data(c.children(root)[i - 1]);
         if !is_bracket(curr.as_str())
             && token_type(curr.as_str()) == token_type(prev.as_str())
             && curr.source == prev.source
             && prev.hi == curr.lo
         {
-            c.tokens.get_mut(c.graph[&root][i - 1]).unwrap().hi = curr.hi;
-            c.graph.get_mut(&root).unwrap().remove(i);
+            c.data_mut(c.children(root)[i - 1]).hi = curr.hi;
+            c.children_mut(root).remove(i);
         } else {
             i += 1;
         }
     }
 }
 
-fn remove_whitespace(c: &mut Context, root: usize) {
+fn remove_whitespace(c: &mut Context, root: Node) {
     let mut i = 0;
-    while i < c.graph[&root].len() {
-        if c.tokens[c.graph[&root][i]]
+    while i < c.children(root).len() {
+        if c.data(c.children(root)[i])
             .as_str()
             .chars()
             .all(char::is_whitespace)
         {
-            c.graph.get_mut(&root).unwrap().remove(i);
+            c.children_mut(root).remove(i);
         } else {
             i += 1;
         }
     }
 }
 
-fn group_brackets(c: &mut Context, root: usize) {
+fn group_brackets(c: &mut Context, root: Node) {
     let match_opener = |token: &Token| match token.as_str() {
         "(" => ")",
         "{" => "}",
@@ -217,9 +243,9 @@ fn group_brackets(c: &mut Context, root: usize) {
     };
     let mut stack = Vec::new();
     let mut i = 0;
-    while i < c.graph[&root].len() {
-        let child = c.graph[&root][i];
-        let token = &c.tokens[child];
+    while i < c.children(root).len() {
+        let child = c.children(root)[i];
+        let token = c.data(child);
         match token.as_str() {
             "(" | "{" | "[" => {
                 stack.push(child);
@@ -228,9 +254,9 @@ fn group_brackets(c: &mut Context, root: usize) {
             ")" | "}" | "]" => match stack.pop() {
                 None => panic!("extra {} at {}\n", token.as_str(), token.location()),
                 Some(popped) => {
-                    let opener = &c.tokens[popped];
+                    let opener = &c.data(popped);
                     if match_opener(opener) == token.as_str() {
-                        c.graph.get_mut(&root).unwrap().remove(i);
+                        c.children_mut(root).remove(i);
                     } else {
                         panic!(
                             "{} matched with {} at {} and {}\n",
@@ -244,8 +270,8 @@ fn group_brackets(c: &mut Context, root: usize) {
             },
             _ if stack.is_empty() => i += 1,
             _ => {
-                let child = c.graph.get_mut(&root).unwrap().remove(i);
-                c.graph.get_mut(stack.last().unwrap()).unwrap().push(child);
+                let child = c.children_mut(root).remove(i);
+                c.children_mut(*stack.last().unwrap()).push(child);
             }
         }
     }
