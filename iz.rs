c476620d0b72e2f665aac4355459dac9e006eb3a
Alex Fischman <alexdfischman@gmail.com>
2023-03-23 20:45:12 -0700

    chars are in Tree

--- iz.rs
+++ iz.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap as Map;
+
 // override for panic macro, comment this out if you need a stack trace of the compiler
 // macro_rules! panic {
 //     () => {{ std::process::exit(-1); }};
@@ -6,27 +8,28 @@
 // }
 
 #[derive(Hash, PartialEq, Eq)]
-struct Source<'a> {
-    name: &'a String,
-    text: &'a String,
+struct Source {
+    name: String,
+    text: String,
 }
 
+use std::rc::Rc; // only needed to get around Any restrictions, not necessary for bootstrapping
 #[derive(Clone, Hash, PartialEq, Eq)]
-struct Token<'a> {
-    source: &'a Source<'a>,
+struct Token {
+    source: Rc<Source>,
     lo: usize,
     hi: usize,
 }
 
 use std::ops::Deref;
-impl Deref for Token<'_> {
+impl Deref for Token {
     type Target = str;
     fn deref(&self) -> &str {
         &self.source.text[self.lo..self.hi]
     }
 }
 
-impl std::fmt::Display for Token<'_> {
+impl std::fmt::Display for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         if self.lo == self.hi {
             return Ok(());
@@ -47,11 +50,48 @@ impl std::fmt::Display for Token<'_> {
     }
 }
 
+use std::any::{Any, TypeId};
+struct Tree {
+    data: Map<TypeId, Box<dyn Any>>,
+    children: Vec<Tree>,
+}
+
+impl Tree {
+    fn new() -> Tree {
+        Tree { data: Map::new(), children: Vec::new() }
+    }
+
+    fn insert<T: Any>(&mut self, x: T) -> Option<T> {
+        self.data.insert(TypeId::of::<T>(), Box::new(x)).map(|any| *any.downcast().unwrap())
+    }
+
+    fn remove<T: Any>(&mut self) -> Option<T> {
+        self.data.remove(&TypeId::of::<T>()).map(|any| *any.downcast().unwrap())
+    }
+
+    fn get<T: Any>(&self) -> Option<&T> {
+        self.data.get(&TypeId::of::<T>()).map(|any| any.downcast_ref().unwrap())
+    }
+
+    fn get_mut<T: Any>(&mut self) -> Option<&mut T> {
+        self.data.get_mut(&TypeId::of::<T>()).map(|any| any.downcast_mut().unwrap())
+    }
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let name = args.get(1).unwrap_or_else(|| panic!("missing .iz file"));
-    let text = &std::fs::read_to_string(name).unwrap_or_else(|_| panic!("could not read {}", name));
-    let source = Source { name, text };
+    let name = args.get(1).unwrap_or_else(|| panic!("missing .iz file")).to_owned();
+    let text = std::fs::read_to_string(&name).unwrap_or_else(|_| panic!("could not read {}", name));
+    let source = Rc::new(Source { name, text });
+
+    let mut tree = Tree::new();
+    let is = source.text.char_indices().map(|(i, _)| i);
+    let js = source.text.char_indices().map(|(j, _)| j);
+    for (i, j) in is.zip(js.skip(1).chain([source.text.len()])) {
+        let mut child = Tree::new();
+        child.insert::<Token>(Token { source: source.clone(), lo: i, hi: j });
+        tree.children.push(child);
+    }
 
     println!("{}", source.text);
 }
