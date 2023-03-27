0b301830fbe2b5eab9ddab6f5626db3730d9e4d2
Alex Fischman <alexdfischman@gmail.com>
2023-03-26 17:36:42 -0700

    added Instruction trait

--- iz.rs
+++ iz.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap as Map;
-
 // override for panic macro, comment this out if you need a stack trace of the compiler
 // macro_rules! panic {
 //     () => {{ std::process::exit(-1); }};
@@ -50,7 +48,8 @@ impl std::fmt::Display for Token {
     }
 }
 
-struct Data(Map<TypeId, Box<dyn Any>>);
+use std::collections::HashMap;
+struct Data(HashMap<TypeId, Box<dyn Any>>);
 impl Data {
     fn insert<T: Any>(&mut self, x: T) -> Option<T> {
         self.0.insert(TypeId::of::<T>(), Box::new(x)).map(|any| *any.downcast().unwrap())
@@ -77,7 +76,7 @@ struct Tree {
 
 impl Tree {
     fn new() -> Tree {
-        Tree { data: Data(Map::new()), children: Vec::new() }
+        Tree { data: Data(HashMap::new()), children: Vec::new() }
     }
 
     fn print(&self, indent: usize) {
@@ -113,12 +112,13 @@ fn main() {
     // parsing
     passes.push(Box::new(match_brackets("(", ")")));
     passes.push(Box::new(match_brackets("{", "}")));
-    passes.push(Box::new(parse_operator("?", Postfix)));
-    passes.push(Box::new(parse_operator(":", Postfix)));
-    passes.push(Box::new(parse_operator("~", Prefix)));
-    passes.push(Box::new(parse_operator("$", Prefix)));
-    passes.push(Box::new(parse_operator("-", Prefix)));
-    passes.push(Box::new(parse_operator("+", InfixLeft)));
+    passes.push(Box::new(parse_operator("?", Operator::Postfix)));
+    passes.push(Box::new(parse_operator(":", Operator::Postfix)));
+    passes.push(Box::new(parse_operator("~", Operator::Prefix)));
+    passes.push(Box::new(parse_operator("$", Operator::Prefix)));
+    passes.push(Box::new(parse_operator("-", Operator::Prefix)));
+    passes.push(Box::new(parse_operator("+", Operator::InfixLeft)));
+    passes.push(Box::new(parse_operator("=", Operator::InfixRight)));
 
     for pass in passes {
         pass(&mut tree);
@@ -191,11 +191,11 @@ fn match_brackets<'a>(open: &'a str, close: &'a str) -> impl Fn(&mut Tree) + 'a 
     }
 }
 
-use Operator::*;
 #[derive(Clone, Copy)]
 enum Operator { Prefix, Postfix, InfixLeft, InfixRight }
 
 fn parse_operator<'a>(name: &'a str, operator: Operator) -> impl Fn(&mut Tree) + 'a {
+    use Operator::*;
     move |tree: &mut Tree| {
         tree.children.iter_mut().for_each(|child| parse_operator(name, operator)(child));
         let mut i = match operator {
@@ -227,4 +227,94 @@ fn parse_operator<'a>(name: &'a str, operator: Operator) -> impl Fn(&mut Tree) +
             };
         }
     }
+}
+
+struct Memory(Vec<i64>);
+
+impl std::ops::Index<i64> for Memory {
+    type Output = i64;
+    fn index(&self, i: i64) -> &i64 {
+        &self.0[i as usize]
+    }
+}
+
+impl std::ops::IndexMut<i64> for Memory {
+    fn index_mut(&mut self, i: i64) -> &mut i64 {
+        let i = i as usize;
+        if i >= self.0.len() {
+            self.0.resize(i + 1, 0)
+        }
+        &mut self.0[i]
+    }
+}
+
+struct Interpreter {
+    labels: HashMap<String, i64>,
+    pc: i64,
+    stack: Memory,
+    sp: i64,
+}
+
+trait Instruction {
+    fn interpret(&self, i: &mut Interpreter);
+}
+
+struct Push(i64);
+impl Instruction for Push {
+    fn interpret(&self, i: &mut Interpreter) {
+        i.sp += 1;
+        i.stack[i.sp] = self.0;
+    }
+}
+
+struct Move(i64);
+impl Instruction for Move {
+    fn interpret(&self, i: &mut Interpreter) {
+        i.sp -= self.0;
+    }
+}
+
+struct Copy(i64);
+impl Instruction for Copy {
+    fn interpret(&self, i: &mut Interpreter) {
+        i.stack[i.sp + 1] = i.stack[i.sp - self.0];
+        i.sp += 1;
+    }
+}
+
+struct Add;
+impl Instruction for Add {
+    fn interpret(&self, i: &mut Interpreter) {
+        i.sp -= 1;
+        i.stack[i.sp] += i.stack[i.sp + 1];
+    }
+}
+
+struct Neg;
+impl Instruction for Neg {
+    fn interpret(&self, i: &mut Interpreter) {
+        i.stack[i.sp] = -i.stack[i.sp];
+    }
+}
+
+struct Ltz;
+impl Instruction for Ltz {
+    fn interpret(&self, i: &mut Interpreter) {
+        i.stack[i.sp] = (i.stack[i.sp] < 0) as i64;
+    }
+}
+
+struct Jumpz(String);
+impl Instruction for Jumpz {
+    fn interpret(&self, i: &mut Interpreter) {
+        if i.stack[i.sp] == 0 {
+            i.pc = *i.labels.get(&self.0).unwrap_or_else(|| panic!("unknown label {}", self.0));
+        }
+        i.sp -= 1;
+    }
+}
+
+struct Label(String);
+impl Instruction for Label {
+    fn interpret(&self, _i: &mut Interpreter) {}
 }
