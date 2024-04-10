7461b19742dd51fc0c39bc9d6fff045913f56f07
Alex Fischman <alexdfischman@gmail.com>
2024-04-09 23:09:15 -0700

    Allow wildcard imports

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 #![allow(clippy::missing_errors_doc)]
 #![allow(clippy::missing_panics_doc)]
 #![allow(clippy::must_use_candidate)]
+#![allow(clippy::wildcard_imports)]
 
 pub mod parse;
 pub mod tree;
