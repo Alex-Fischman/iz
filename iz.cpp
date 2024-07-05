894fed41efe1d251c7de3c7c1d062dfbb4fd71b4
Alex Fischman <alexdfischman@gmail.com>
2024-07-04 18:25:47 -0700

    Usage message

--- iz.cpp
+++ iz.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-	printf("Hello, world!\n");
+	if (argc != 2) {
+		printf("usage: ./iz [file.iz]\n");
+		return 1;
+	}
+
 	return 0;
 }
