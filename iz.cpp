b80f7949170072b0a73934202d29b368dc149bc4
Alex Fischman <alexdfischman@gmail.com>
2024-07-04 20:22:41 -0700

    Add file read, print, close

--- iz.cpp
+++ iz.cpp
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
@@ -5,6 +6,15 @@ int main(int argc, char const *argv[]) {
 		printf("usage: ./iz [file.iz]\n");
 		return 1;
 	}
+
+	FILE* file = fopen(argv[1], "r");
+
+	char buffer;
+	while (fread(&buffer, 1, 1, file)) printf("%c", buffer);
+
+	fclose(file);
+
+	printf("\n");
 
 	return 0;
 }
