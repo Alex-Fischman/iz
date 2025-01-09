1ce72d8c463ec36a1b78b9e1afdaa18012c8a4fb
Alex Fischman <alexdfischman@gmail.com>
2025-01-09 10:51:43 -0800

    Fix extra clone with Spans iterator

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -1,30 +1,50 @@
 use crate::*;
 
+/// Like `str::char_indices` but doesn't hold a reference to the string.
+struct Spans {
+    /// Which source to use. Should not change.
+    source: usize,
+    /// Index of `lo` for the current span.
+    lo: usize,
+}
+
+impl Spans {
+    fn peek(&self, state: &State) -> Option<Span> {
+        let text = &state.sources[self.source].text[self.lo..];
+        Some(Span {
+            lo: self.lo,
+            hi: self.lo + text.chars().next()?.len_utf8(),
+            source: self.source,
+        })
+    }
+
+    fn next(&mut self, state: &State) -> Option<Span> {
+        let span = self.peek(state);
+        if let Some(span) = span {
+            self.lo = span.hi;
+        }
+        span
+    }
+}
+
 /// Tokenizes the given source file, adding its tokens as children to
 /// the given parent node.
 #[allow(clippy::missing_errors_doc, clippy::missing_panics_doc)]
 pub fn tokenize(state: &mut State, src: Source, parent: usize) -> Result<()> {
     let source = state.sources.len();
-    state.sources.push(Source {
-        name: src.name,
-        text: src.text.clone(),
-    });
+    state.sources.push(src);
 
-    let los = src.text.char_indices().map(|(i, _)| i);
-    let his = los.clone().skip(1).chain([src.text.len()]);
-    let spans = los.zip(his).map(|(lo, hi)| Span { source, lo, hi });
-    let mut spans = spans.peekable();
+    let mut spans = Spans { source, lo: 0 };
 
-    while let Some(mut span) = spans.next() {
+    while let Some(mut span) = spans.next(state) {
         assert_eq!(span.string(state).chars().count(), 1);
         let tag = match span.single_char(state) {
             c if c.is_whitespace() => continue,
             '#' => {
-                while let Some(s) = spans.peek() {
+                while let Some(s) = spans.next(state) {
                     if s.single_char(state) == '\n' {
                         break;
                     }
-                    spans.next();
                 }
                 continue;
             }
@@ -36,7 +56,7 @@ pub fn tokenize(state: &mut State, src: Source, parent: usize) -> Result<()> {
                 let mut in_escape = false;
                 let mut string = String::new();
                 loop {
-                    let Some(s) = spans.next() else {
+                    let Some(s) = spans.next(state) else {
                         return err!(state, span, "no matching end quote");
                     };
                     if in_escape {
@@ -63,14 +83,14 @@ pub fn tokenize(state: &mut State, src: Source, parent: usize) -> Result<()> {
                 Tag::String(string)
             }
             _ => {
-                while let Some(s) = spans.peek() {
+                while let Some(s) = spans.peek(state) {
                     let c = s.single_char(state);
                     let is_special = matches!(c, '#' | '(' | ')' | '{' | '}' | '[' | ']' | '"');
                     if c.is_whitespace() || is_special {
                         break;
                     }
                     span.hi = s.hi;
-                    spans.next();
+                    spans.next(state);
                 }
                 Tag::Identifier
             }
