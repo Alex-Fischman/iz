db3bcee3340eb1de12fe421ee3cd547a0103190f
Alex Fischman <alexdfischman@gmail.com>
2025-05-04 00:17:16 -0700

    Restart again, again, and again until it is done

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,42 @@
-//! A compiler for the Iz programming language.
+//! A compiler for the `iz` programming language.
 
 #![deny(clippy::all, clippy::pedantic, missing_docs)]
+#![allow(clippy::missing_errors_doc)]
 
 pub use std::collections::HashMap;
 pub use std::fmt::{Debug, Display, Formatter};
 use std::process::ExitCode;
 
-mod bracket;
-mod state;
-mod tokenize;
-pub use bracket::*;
-pub use state::*;
-pub use tokenize::*;
-
 /// Global `Result` alias for ease of use. See the `err!` macro.
 pub type Result<T> = std::result::Result<T, String>;
 
-/// Constructs an error message from the given state, span,
-/// and format arguments.
-#[macro_export]
-macro_rules! err {
-    ($state:expr, $span:expr, $($fmt:tt)*) => {
-        Err(format!(
-            "error at {}: {}\n{}",
-            $span.location($state),
-            format!($($fmt)*),
-            $span.string($state),
-        ))
-    };
+/// Run the compiler, given the source code and an optional source name.
+#[allow(clippy::needless_pass_by_value)]
+pub fn run(text: String, name: Option<String>) -> Result<()> {
+    if let Some(name) = name {
+        println!("{name}\n");
+    }
+    println!("{text}");
+    Ok(())
+}
+
+/// Run the compiler, given a source file name.
+pub fn run_file(name: String) -> Result<()> {
+    match std::fs::read_to_string(&name) {
+        Ok(text) => run(text, Some(name)),
+        Err(_) => Err(format!("could not read {name}")),
+    }
+}
+
+/// Run the compiler, given the command line arguments.
+pub fn run_args() -> Result<()> {
+    let args: Vec<_> = std::env::args().collect();
+    let name = args.get(1).ok_or("usage: pass a .iz file")?.to_string();
+    run_file(name)
 }
 
 fn main() -> ExitCode {
-    fn main() -> Result<()> {
-        let args: Vec<_> = std::env::args().collect();
-        let name = args.get(1).ok_or("usage: pass a .iz file")?.to_string();
-        let text = std::fs::read_to_string(&name).map_err(|_| format!("could not read {name}"))?;
-        run(Source { name, text })
-    }
-    match main() {
+    match run_args() {
         Ok(()) => ExitCode::SUCCESS,
         Err(e) => {
             eprintln!("{e}");
@@ -46,65 +45,12 @@ fn main() -> ExitCode {
     }
 }
 
-/// Represents a file containing source code.
-pub struct Source {
-    /// The file name.
-    pub name: String,
-    /// The file contents.
-    pub text: String,
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-/// A snippet of a `Source`.
-#[derive(Clone, Copy)]
-pub struct Span {
-    /// Which source file this span refers to.
-    pub source: usize,
-    /// The byte index of the first character in the snippet.
-    pub lo: usize,
-    /// The byte index after the last character in the snippet.
-    pub hi: usize,
-}
-
-impl Span {
-    /// Get the text from the snippet.
-    #[must_use]
-    pub fn string(self, state: &State) -> &str {
-        &state.sources[self.source].text[self.lo..self.hi]
+    #[test]
+    fn empty_program_ok() -> Result<()> {
+        run(String::new(), None)
     }
-
-    /// Assert that the snippet is a single `char` and return it.
-    /// # Panics
-    /// Will panic if this `Span` contains more or less than 1 `char`.
-    #[must_use]
-    pub fn single_char(self, state: &State) -> char {
-        assert_eq!(self.string(state).chars().count(), 1);
-        self.string(state).chars().next().unwrap()
-    }
-
-    /// Get the location of the snippet.
-    #[must_use]
-    pub fn location(&self, state: &State) -> String {
-        let source = &state.sources[self.source];
-        let mut row = 1;
-        let mut col = 1;
-        for (i, c) in source.text.char_indices() {
-            match (i, c) {
-                (i, _) if i == self.lo => break,
-                (_, '\n') => {
-                    row += 1;
-                    col = 1;
-                }
-                _ => col += 1,
-            }
-        }
-        format!("{}:{row}:{col}", source.name)
-    }
-}
-
-fn run(source: Source) -> Result<()> {
-    let mut state = State::default();
-    tokenize(&mut state, source, ROOT)?;
-    bracket(&mut state, ROOT)?;
-    print!("{state:?}");
-    Ok(())
 }
